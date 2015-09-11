f87acd9ffa9b9f35acc665d1fb1a75fe7f7327c8
Shota Narumi <k013c1112@it-neec.jp>
2015-09-11 11:23:53 +0900

    issue#9　index.phpの作成

---
+++ fuel/app/views/index.php
@@ -0,0 +1,7 @@
+<div id="week-select" class="visible-xs">
+    <button type="button" class="btn btn-default">月</button>
+    <button type="button" class="btn btn-default">火</button>
+    <button type="button" class="btn btn-default">水</button>
+    <button type="button" class="btn btn-default">木</button>
+    <button type="button" class="btn btn-default">金</button>
+</div>
