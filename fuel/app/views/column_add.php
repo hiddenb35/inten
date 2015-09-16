ce13e6f4957b2e5e39822bb3ab8a05629a39ad45
Shota Narumi <k013c1112@it-neec.jp>
2015-09-16 23:55:52 +0900

    issue#42 授業の登録画面の更なる修正

---
+++ fuel/app/views/column_add.php
@@ -0,0 +1,48 @@
+<div id="column_add">
+    <form class="form-horizontal">
+        <div class="form-group">
+            <label class="control-label" for="column">授業名</label>
+            <div class="controls">
+                <input type="text" class="input-xlarge" id="column">
+            </div>
+        </div>
+
+        <label class="radio-inline">
+            <input type="radio" name="optionsRadios" id="semester1" value="">前期
+        </label>
+        <label class="radio-inline">
+            <input type="radio" name="optionsRadios" id="semester2" value="">後期
+        </label>
+
+        <div class="form-group">
+            <label class="control-label" for="unit">総単位数</label>
+            <div class="controls">
+                <input type="text" class="input-mini" id="unit">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="control-label" for="date">作成日時</label>
+            <div class="controls">
+                <input type="text" class="input-xlarge" id="date">
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label class="control-label" for="update">更新日時</label>
+            <div class="controls">
+                <input type="text" class="input-xlarge" id="update">
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label class="control-label" for="classid">クラスID</label>
+            <div class="controls">
+                <input type="text" class="input-xlarge" id="class_id">
+            </div>
+        </div>
+        
+        <button type="button" class="btn btn-primary">登録</button>
+        <button type="reset" class="btn btn-warning">キャンセル</button>
+    </form>
+</div>
