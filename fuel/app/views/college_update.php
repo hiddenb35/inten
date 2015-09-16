6c81f410fbeadc48463a88c900ed48a02d4c70b8
Shota Narumi <k013c1112@it-neec.jp>
2015-09-17 00:21:20 +0900

    issue#23 college_update.phpの作成

---
+++ fuel/app/views/college_update.php
@@ -0,0 +1,14 @@
+<div id="college_update">
+    <form class="form-horizontal">
+        <div class="form-group">
+            <label for="college_name" class="control-label">カレッジ名</label>
+            <div class="controls">
+                <input type="text" class="input-xxlarge" id="college_name">
+            </div>
+        </div>
+        <div class="form-group">
+            <button type="button" class="btn btn-primary">登録</button>
+            <button type="reset" class="btn btn-warning">キャンセル</button>
+        </div>
+    </form>
+</div>
