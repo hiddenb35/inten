cac2bab7f33660de9880987bff98346235c38350
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-09-17 00:27:02 +0900

    issue#2 教員用ログインメソッドの作成

--- fuel/app/classes/controller/auth.php
+++ fuel/app/classes/controller/auth.php
@@ -1,6 +1,5 @@
 <?php
 
-// todo Viewができ次第、生徒用・教員用に対応
 class Controller_Auth extends Controller_Based
 {
 	public function action_slogin()
@@ -23,6 +22,29 @@ class Controller_Auth extends Controller_Based
 		else
 		{
 			Response::redirect('auth/slogin');
+		}
+	}
+
+	public function action_tlogin()
+	{
+		$this->template->header->set('title', '教員用ログインページ');
+		$this->template->content = View::forge('auth/teacher_login');
+	}
+
+	public function post_tlogin()
+	{
+		$username = Input::post('username');
+		$password = Input::post('password');
+
+		$auth = Auth::instance('teacherauth');
+
+		if($auth->login($username, $password))
+		{
+			Response::redirect('index/index');
+		}
+		else
+		{
+			Response::redirect('auth/tlogin');
 		}
 	}
 }
