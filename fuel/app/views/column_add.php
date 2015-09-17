661886125cf29a01b8d52b5cf8c463b8aec2b125
Shota Narumi <k013c1112@it-neec.jp>
2015-09-17 11:41:10 +0900

    issue#46 column_add.phpにsection部分の追加

--- fuel/app/views/column_add.php
+++ fuel/app/views/column_add.php
@@ -1,3 +1,13 @@
+<section class="content-header">
+    <h1>
+        授業追加
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    </ol>
+</section>
+<!-- Main content -->
+<section class="content">
 <div id="column_add">
     <form class="form-horizontal">
         <div class="form-group">
@@ -47,3 +57,4 @@
 
     </form>
 </div>
+</section>
