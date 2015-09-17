c89622a629695ad106af4ea2373db357f68a76d5
Shota Narumi <k013c1112@it-neec.jp>
2015-09-17 15:32:13 +0900

    issue#46 student_login.phpの修正

--- fuel/app/views/auth/student_login.php
+++ fuel/app/views/auth/student_login.php
@@ -1,27 +1,30 @@
+<section class="content-header">
+    <h1>
+        ログイン
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    </ol>
+</section>
+<!-- Main content -->
+<section class="content">
 <div id="login">
-	<h1>ログイン</h1>
-	<hr>
-	<div>
-		<table>
-			<form action="/auth/slogin" method="post" value="">
-				<tr>
-					<th>
-						ユーザーID
-					</th>
-					<td>
-						<input type="text" name="username" value="">
-					</td>
-				</tr>
-				<tr>
-					<th>
-						パスワード
-					</th>
-					<td>
-						<input type="password" name="password" value="">
-					</td>
-				</tr>
-				<button type="submit">ログイン</button>
-			</form>
-		</table>
-	</div>
+    <form class="form-horizontal">
+        <div class="form-group">
+            <div class="input-prepend">
+                <span class="add-on"><i class="ion-social-github"></i></span>
+                <input type="text" class="input-xlarge" id="user" name="username" placeholder="ユーザ名">
+            </div>                    
+        </div>
+        <div class="form-group">
+            <div class="input-prepend">
+                <span class="add-on"><i class="ion-android-lock"></i></span>
+                <input type="text" class="input-xlarge" id="pass" name="password" placeholder="パスワード">
+            </div>
+        </div>
+
+        <button type="button" class="btn btn-primary">ログイン</button>
+ 
+    </form>
 </div>
+</section>
