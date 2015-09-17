abe72a4a9cb5819ac83a108e62c78986f820a862
Shota Narumi <k013c1112@it-neec.jp>
2015-09-17 15:42:43 +0900

    issue#46 pass_update.phpの修正

--- fuel/app/views/auth/pass_update.php
+++ fuel/app/views/auth/pass_update.php
@@ -1,23 +1,33 @@
+<section class="content-header">
+    <h1>
+        パスワード変更
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    </ol>
+</section>
+<!-- Main content -->
+<section class="content">
 <div id="pass_update">
     <form class="form-horizontal">
         <div class="form-group">
-            <label for="now_pass" class="control-label">現在のパスワード</label>
+            <label for="current_pass" class="control-label">現在のパスワード</label>
             <div class="controls">
-                <input type="text" class="input-xlarge" id="now_pass">
+                <input type="text" class="input-xlarge" id="current_pass" name="current_password">
             </div>
         </div>
 
         <div class="form-group">
             <label for="new_pass" class="control-label">新規パスワード</label>
             <div class="controls">
-                <input type="password" class="input-xlarge" id="new_pass">
+                <input type="password" class="input-xlarge" id="new_pass" name="new_password">
             </div>
         </div>
 
         <div class="form-group">
             <label for="check_pass" class="control-label">新規パスワード(確認)</label>
             <div class="controls">
-                <input type="password" class="input-xlarge" id="check_pass">
+                <input type="password" class="input-xlarge" id="check_pass" name="new_password_confirm">
             </div>
         </div>
 
@@ -27,3 +37,4 @@
         </div>
     </form>
 </div>
+</section>
