d0b325876205edae18acf5f0fac1d3db2370f828
Shota Narumi <k013c1112@it-neec.jp>
2015-09-17 09:49:35 +0900

    issue#23 college_list.phpの作成

---
+++ fuel/app/views/college_list.php
@@ -0,0 +1,25 @@
+<div id="college_list">
+    <div class="table-responsive">
+        <table class="table table-striped table-hover" style="border :1px #000000;">
+            <thead>
+                <tr>
+                    <th>カレッジ名</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>ITカレッジ</td>
+                </tr>
+                <tr>
+                    <td>クリエイターズカレッジ</td>
+                </tr>
+                <tr>
+                    <td>ミュージックカレッジ</td>
+                </tr>
+                <tr>
+                    <td>テクノロジーカレッジ</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
