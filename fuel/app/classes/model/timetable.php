6c01cac7387595937e357823e07a9c2e80ae3ca3
k013c1118 <k013c1118@it-neec.jp>
2015-09-17 14:30:59 +0900

    issue#8 timetableテーブルのORMモデルの作成

---
+++ fuel/app/classes/model/timetable.php
@@ -0,0 +1,52 @@
+<?php
+
+Class Model_Timetable extends \Orm\Model
+{
+	protected static $_table_name = 'timetable';
+	protected static $_primary_key = array('id');
+
+	protected static $_properties = array(
+		'id',
+		'name' => array(
+			'data_type' => 'varchar',
+		),
+		'html' => array(
+			'data_type' => 'html',
+		),
+		'class_id' => array(
+			'data_type' => 'int',
+		),
+		'is_active' => array(
+			'data_type' => 'int',
+		),
+		'created_at' => array(
+			'data_type' => 'int',
+		),
+		'updated_at' => array(
+			'data_type' => 'int',
+		),
+	);
+
+	protected static $_observers = array(
+		'Orm\Observer_CreatedAt' => array(
+			'events' => array('before_insert'),
+			'mysql_timestamp' => false,
+			'property' => 'created_at',
+		),
+		'Orm\Observer_UpdatedAt' => array(
+			'events' => array('before_update'),
+			'mysql_timestamp' => false,
+			'property' => 'updated_at',
+		),
+	);
+
+	protected static $_belongs_to = array(
+		'class' => array(
+			'model_to' => 'Model_Class',
+			'key_from' => 'class_id',
+			'key_to' => 'id',
+			'cascade_save' => false,
+			'cascade_delete' => false,
+		),
+	);
+}
