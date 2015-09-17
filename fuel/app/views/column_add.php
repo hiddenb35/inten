74d8a3d6cbebe6cf13c3a73aaf78a51849d55e33
Shota Narumi <k013c1112@it-neec.jp>
2015-09-17 12:02:05 +0900

    issue#46 column_add.phpの修正

--- fuel/app/views/column_add.php
+++ fuel/app/views/column_add.php
@@ -13,45 +13,46 @@
         <div class="form-group">
             <label class="control-label" for="column">授業名</label>
             <div class="controls">
-                <input type="text" class="input-xlarge" id="column">
+                <input type="text" class="input-xlarge" id="column" name="name">
             </div>
         </div>
 
         <label class="radio-inline">
-            <input type="radio" name="optionsRadios" id="semester1" value="">前期
+            <input type="radio" id="semester1" name="term">前期
         </label>
         <label class="radio-inline">
-            <input type="radio" name="optionsRadios" id="semester2" value="">後期
+            <input type="radio" id="semester2" name="term">後期
         </label>
 
         <div class="form-group">
             <label class="control-label" for="unit">総単位数</label>
             <div class="controls">
-                <input type="text" class="input-mini" id="unit">
+                <input type="text" class="input-mini" id="unit" name="sum_credit">
+            </div>
+        </div>
+       
+        <div class="form-group">
+            <label class="control-label" for="class_id">クラスID</label>
+            <div class="controls">
+                <div>
+                    <select name="class_id" size="1">
+                        <option value="サンプル1">サンプル1</option><!-- 芦沢ゾーン -->
+                        <option value="サンプル1">サンプル1</option><!-- 芦沢ゾーン -->
+                        <option value="サンプル1">サンプル1</option><!-- 芦沢ゾーン -->
+                        <option value="サンプル1">サンプル1</option><!-- 芦沢ゾーン -->
+                        <option value="サンプル1">サンプル1</option><!-- 芦沢ゾーン -->
+                        <option value="サンプル1">サンプル1</option><!-- 芦沢ゾーン -->
+                        <option value="サンプル1">サンプル1</option><!-- 芦沢ゾーン -->
+                        <option value="サンプル1">サンプル1</option><!-- 芦沢ゾーン -->
+                        <option value="サンプル1">サンプル1</option><!-- 芦沢ゾーン -->
+                        <option value="サンプル1">サンプル1</option><!-- 芦沢ゾーン -->
+                        <option value="サンプル1">サンプル1</option><!-- 芦沢ゾーン -->
+                    </select>
+                </div>
+
             </div>
         </div>
 
-        <div class="form-group">
-            <label class="control-label" for="date">作成日時</label>
-            <div class="controls">
-                <input type="text" class="input-xlarge" id="date">
-            </div>
-        </div>
-        
-        <div class="form-group">
-            <label class="control-label" for="update">更新日時</label>
-            <div class="controls">
-                <input type="text" class="input-xlarge" id="update">
-            </div>
-        </div>
-        
-        <div class="form-group">
-            <label class="control-label" for="classid">クラスID</label>
-            <div class="controls">
-                <input type="text" class="input-xlarge" id="class_id">
-            </div>
-        </div>
-        
         <button type="button" class="btn btn-primary">登録</button>
         <button type="reset" class="btn btn-warning">キャンセル</button>
 
