04137ff5e741db5e01cc395b152c1034b5b3fce0
Keita Kasai <k013c1345@it-neec.jp>
2015-09-17 02:36:40 +0000

    issue#37 header部分にUser関係の項目を追加しました。

--- fuel/app/views/parts/header.php
+++ fuel/app/views/parts/header.php
@@ -64,5 +64,30 @@
 							'data-toggle' => 'offcanvas',
 							'role' => 'button'));
 				?>
+				<div class="navbar-custom-menu">
+					<ul class="nav navbar-nav">
+						<!-- 案１ -->
+						<li class="dropdown user user-menu">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+								<span class="hidden-xs">田中太郎</span>
+							</a>
+							<ul class="dropdown-menu">
+								<li class="user-header">
+									<p>
+										K013C0000 - 田中太郎
+									</p>
+								</li>
+								<li class="user-footer">
+									<div class="pull-left">
+										<a href="" class="btn btn-default btn-flat">プロフィール</a>
+									</div>
+									<div class="pull-right">
+										<a href="/auth/logput" class="btn btn-default btn-flat">ログアウト</a>
+									</div>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
 			</nav>
 		</header>
