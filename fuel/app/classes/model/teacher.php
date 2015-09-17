d9b7edf98a414e2b02aabae60a474621b69b3973
k013c1118 <k013c1118@it-neec.jp>
2015-09-17 14:30:37 +0900

    issue#8 teacherテーブルのORMモデルの作成

---
+++ fuel/app/classes/model/teacher.php
@@ -0,0 +1,79 @@
+<?php
+
+Class Model_Teacher extends \Orm\Model
+{
+	protected static $_table_name = 'teacher';
+	protected static $_primary_key = array('id');
+
+	protected static $_properties = array(
+		'id',
+		'username' => array(
+			'data_type' => 'varchar',
+		),
+		'birthday' => array(
+			'data_type' => 'date',
+		),
+		'password' => array(
+			'data_type' => 'varchar',
+		),
+		'first_name' => array(
+			'data_type' => 'varchar',
+		),
+		'first_name_kana' => array(
+			'data_type' => 'varchar',
+		),
+		'last_name' => array(
+			'data_type' => 'varchar',
+		),
+		'last_name_kana' => array(
+			'data_type' => 'varchar',
+		),
+		'email' => array(
+			'data_type' => 'varchar',
+		),
+		'gender' => array(
+			'data_type' => 'int',
+		),
+		'group' => array(
+			'data_type' => 'int',
+		),
+		'last_login' => array(
+			'data_type' => 'varchar',
+		),
+		'profile_fields' => array(
+			'data_type' => 'text',
+		),
+		'login_hash' => array(
+			'data_type' => 'varchar',
+		),
+		'created_at' => array(
+			'data_type' => 'int',
+		),
+		'updated_at' => array(
+			'data_type' => 'int',
+		),
+	);
+
+	protected static $_observers = array(
+		'Orm\Observer_CreatedAt' => array(
+			'events' => array('before_insert'),
+			'mysql_timestamp' => false,
+			'property' => 'created_at',
+		),
+		'Orm\Observer_UpdatedAt' => array(
+			'events' => array('before_update'),
+			'mysql_timestamp' => false,
+			'property' => 'updated_at',
+		),
+	);
+
+	protected static $_has_many = array(
+		'attachment' => array(
+			'model_to' => 'Model_Attachment',
+			'key_from' => 'id',
+			'key_to' => 'teacher_id',
+			'cascade_save' => false,
+			'cascade_delete' => false,
+		),
+	);
+}
