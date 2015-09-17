13429a7cca034aeda6e301f64759b63082afad7c
k013c1118 <k013c1118@it-neec.jp>
2015-09-17 14:29:46 +0900

    issue#8 courseテーブルのORMモデルの作成

---
+++ fuel/app/classes/model/course.php
@@ -0,0 +1,56 @@
+<?php
+
+Class Model_Course extends \Orm\Model
+{
+	protected static $_table_name = 'course';
+	protected static $_primary_key = array('id');
+
+	protected static $_properties = array(
+		'id',
+		'name' => array(
+			'data_type' => 'varchar',
+		),
+		'college_id' => array(
+			'data_type' => 'int',
+		),
+		'created_at' => array(
+			'data_type' => 'int',
+		),
+		'updated_at' => array(
+			'data_type' => 'int',
+		),
+	);
+
+	protected static $_observers = array(
+		'Orm\Observer_CreatedAt' => array(
+			'events' => array('before_insert'),
+			'mysql_timestamp' => false,
+			'property' => 'created_at',
+		),
+		'Orm\Observer_UpdatedAt' => array(
+			'events' => array('before_update'),
+			'mysql_timestamp' => false,
+			'property' => 'updated_at',
+		),
+	);
+
+	protected static $_has_many = array(
+		'class' => array(
+			'model_to' => 'Model_Class',
+			'key_from' => 'id',
+			'key_to' => 'course_id',
+			'cascade_save' => false,
+			'cascade_delete' => false,
+		),
+	);
+
+	protected static $_belongs_to = array(
+		'college' => array(
+			'model_to' => 'Model_College',
+			'key_from' => 'college_id',
+			'key_to' => 'id',
+			'cascade_save' => false,
+			'cascade_delete' => false,
+		),
+	);
+}
