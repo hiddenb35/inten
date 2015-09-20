60f3b3921eb07ecf782375a988862fab69feb333
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-09-20 20:48:07 +0900

    issue#60 パスワード変更メソッドの仮作成
    パスワード変更メソッドを仮に作成
    実際にパスワードは変更されない

--- fuel/app/classes/controller/auth.php
+++ fuel/app/classes/controller/auth.php
@@ -81,4 +81,46 @@ class Controller_Auth extends Controller_Based
 			throw new HttpNotFoundException;
 		}
 	}
+
+	public function action_pass_update()
+	{
+		if(!Auth::check())
+		{
+			throw new HttpNotFoundException;
+		}
+
+		$title = 'パスワード変更';
+
+		if(Input::method() === 'POST')
+		{
+			list($auth) = array_values(Auth::verified());
+
+			$val = Validation::forge();
+
+			// todo 現在のパスワードの検証
+			$val->add('current_password', '現在のパスワード')
+				->add_rule('required');
+			$val->add('new_password', '新規パスワード')
+				->add_rule('required')
+				->add_rule('min_length', 4)
+				->add_rule('max_length', 255);
+			$val->add('new_password_confirm', '新規パスワード(確認)')
+				->add_rule('required')
+				->add_rule('match_field', 'new_password');
+
+			if($val->run())
+			{
+				// todo 実際のパスワード変更処理
+				$title = 'パスワードを変更しました';
+			}
+			else
+			{
+				// todo viewへのエラーメッセージのセット
+				$title = 'パスワード変更エラー';
+			}
+		}
+
+		$this->template->title = $title;
+		$this->template->content = View::forge('auth/pass_update');
+	}
 }
