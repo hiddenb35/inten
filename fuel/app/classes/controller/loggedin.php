7d8975879a032e45dff30b4c6b2fa6cbe495e93b
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-09-20 23:14:43 +0900

    issue#60 Loggedinコントローラの機能を拡張
    ・ログインしているユーザ情報の取得するヘルパーメソッドを実装
    ・ログインしているユーザ情報を全てのviewで見れるように設定

--- fuel/app/classes/controller/loggedin.php
+++ fuel/app/classes/controller/loggedin.php
@@ -2,15 +2,80 @@
 
 class Controller_Loggedin extends Controller_Based
 {
+	/**
+	 * @var \Auth\Auth_Driver $auth
+	 */
+	private $auth = null;
+
 	public function before()
 	{
 		parent::before();
-		$sauth = Auth::instance('studentauth');
-		$tauth = Auth::instance('teacherauth');
 
-		if (( ! $sauth->check()) && ( ! $tauth->check()))
+		if (!Auth::check())
 		{
 			Response::redirect('auth/slogin');
 		}
+
+		list($this->auth) = array_values(Auth::verified());
+
+		$user_info = array(
+			'user_number' => $this->get_user_number(),
+			'full_name' => $this->get_full_name(),
+			'full_name_kana' => $this->get_full_name_kana(),
+			'email' => $this->get_email(),
+		);
+		$this->template->set_global('user_info', $user_info);
+
 	}
+
+	public function is_student()
+	{
+		return ($this->auth->get_id() === 'studentauth') ? true : false;
+	}
+
+	public function is_teacher()
+	{
+		return ($this->auth->get_id() === 'teacherauth') ? true : false;
+	}
+
+	public function is_charge()
+	{
+		if(!$this->is_teacher())
+		{
+			return false;
+		}
+
+		return ($this->auth->has_access('charge.ok')) ? true : false;
+	}
+
+	public function is_admin()
+	{
+		if(!$this->is_teacher())
+		{
+			return false;
+		}
+
+		return ($this->auth->has_access('admin.ok')) ? true : false;
+	}
+
+	public function get_user_number()
+	{
+		return $this->auth->get('username');
+	}
+
+	public function get_full_name()
+	{
+		return $this->auth->get('last_name') . ' ' .$this->auth->get('first_name');
+	}
+
+	public function get_full_name_kana()
+	{
+		return $this->auth->get('last_name_kana') . ' ' . $this->auth->get('first_name_kana');
+	}
+
+	public function get_email()
+	{
+		return $this->auth->get('email');
+	}
+
 }
