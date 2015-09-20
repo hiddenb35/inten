06bb540ea12f218f0106b3f2491ff5a279689fa6
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-09-20 19:36:40 +0900

    issue#60 ログインメソッドの統合
    GET通信とPOST通信で振り分けていたメソッドを
    １つのメソッドに統合

--- fuel/app/classes/controller/auth.php
+++ fuel/app/classes/controller/auth.php
@@ -4,48 +4,47 @@ class Controller_Auth extends Controller_Based
 {
 	public function action_slogin()
 	{
+		if(Input::method() === 'POST')
+		{
+			$username = Input::post('username');
+			$password = Input::post('password');
+			$auth = Auth::instance('studentauth');
+
+			if($auth->login($username, $password))
+			{
+				Response::redirect('/');
+			}
+			else
+			{
+				Response::redirect('auth/slogin');
+			}
+		}
+
 		$this->template->title = '生徒用ログインページ';
 		$this->template->content = View::forge('auth/student_login');
 	}
 
-	public function post_slogin()
-	{
-		$username = Input::post('username');
-		$password = Input::post('password');
-
-		$auth = Auth::instance('studentauth');
-
-		if($auth->login($username, $password))
-		{
-			Response::redirect('index/index');
-		}
-		else
-		{
-			Response::redirect('auth/slogin');
-		}
-	}
-
 	public function action_tlogin()
 	{
+		if(Input::method() === 'POST')
+		{
+			$username = Input::post('username');
+			$password = Input::post('password');
+
+			$auth = Auth::instance('teacherauth');
+
+			if($auth->login($username, $password))
+			{
+				Response::redirect('/');
+			}
+			else
+			{
+				Response::redirect('auth/tlogin');
+			}
+		}
+
 		$this->template->title = '教員用ログインページ';
 		$this->template->content = View::forge('auth/teacher_login');
-	}
-
-	public function post_tlogin()
-	{
-		$username = Input::post('username');
-		$password = Input::post('password');
-
-		$auth = Auth::instance('teacherauth');
-
-		if($auth->login($username, $password))
-		{
-			Response::redirect('index/index');
-		}
-		else
-		{
-			Response::redirect('auth/tlogin');
-		}
 	}
 
 	public function action_logout()
