1e683c008089e2c368f2fd376b00b8b925a4435f
Shota Narumi <k013c1112@it-neec.jp>
2015-09-22 01:39:36 +0900

    issue#23 college_add.phpの編集

--- fuel/app/views/college_add.php
+++ fuel/app/views/college_add.php
@@ -1,14 +1,25 @@
-<div id="college_add">
+<section class="content-header">
+    <h1>
+        カレッジ登録
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    </ol>
+</section>
+<!-- Main content -->
+<section>
+<div id="COLLEGE_ADD">
     <form class="form-horizontal">
         <div class="form-group">
-            <label for="college_name" class="control-label">カレッジ名</label>
+            <label for="name" class="control-label">カレッジ名</label>
             <div class="controls">
-                <input type="text" class="input-xxlarge" id="college_name" name="name">
+                <input type="text" class="input-xxlarge" id="name" name="name">
             </div>
         </div>
         <div class="form-group">
-            <button type="button" class="btn btn-primary">登録</button>
+            <button type="submit" class="btn btn-primary">登録</button>
             <button type="reset" class="btn btn-warning">キャンセル</button>
         </div>
     </form>
 </div>
+</section>
