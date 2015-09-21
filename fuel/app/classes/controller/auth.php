3dfbdad6190446b41df7228c88d7689ca82fd19a
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-09-22 00:35:17 +0900

    issue#67 ログイン前用のファイル群の配置
    ・ログイン前用のテンプレートファイルを配置
    ・ログイン前用のCSSファイルを配置
    ・authコントローラをログイン前用に変更

--- fuel/app/classes/controller/auth.php
+++ fuel/app/classes/controller/auth.php
@@ -2,13 +2,7 @@
 
 class Controller_Auth extends Controller_Template
 {
-	public function before()
-	{
-		parent::before();
-		$this->template->header  = View::forge('parts/header');
-		$this->template->sidebar = View::forge('parts/sidebar');
-		$this->template->footer  = View::forge('parts/footer');
-	}
+	public $template = 'template/before_login/template';
 
 	public function action_slogin()
 	{
