53067001dc1b6503d97550a63849d99c79334908
Shota Narumi <k013c1112@it-neec.jp>
2015-09-22 02:23:14 +0900

    issue#25 class_edit.phpの修正

--- fuel/app/views/class_edit.php
+++ fuel/app/views/class_edit.php
@@ -1,6 +1,6 @@
 <section class="content-header">
     <h1>
-        クラス登録
+        クラス編集
     </h1>
     <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
@@ -9,12 +9,12 @@
 <!-- Main content -->
 <section class="content">
 <div id="CLASS_EDIT">
-    <form class="form-horizontal">
+    <form class="form-horizontal" method="post">
 
         <div class="form-group">
-            <label for="class_name" class="control-label">クラス名</label>
+            <label for="name" class="control-label">クラス名</label>
             <div class="controls">
-                <input type="text" class="input-xlarge" id="class_name" name="name">
+                <input type="text" class="input-xlarge" id="name" name="name">
             </div>
         </div>
 
