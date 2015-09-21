ddceadc5f9996f3fa415bdcb30951552abbb3179
Keita Kasai <k013c1345@it-neec.jp>
2015-09-21 23:53:08 +0000

    issue#75 サイドバーのカレッジ関係のメニューを追加しました。

--- fuel/app/views/template/after_login/sidebar.php
+++ fuel/app/views/template/after_login/sidebar.php
@@ -8,6 +8,16 @@
 				<i class="fa ion-clock"></i> <span>時間割表示</span>
 			</a>
 		</li>
+		<li class="treeview">
+			<a href="/">
+				<i class="fa ion-university"></i> <span>カレッジ</span> <i class="fa fa-angle-down pull-right"></i>
+			</a>
+			<ul class="treeview-menu">
+				<li><a href="/college/list"><i class="fa fa-circle-o"></i>カレッジ一覧</a></li>
+				<li><a href="/college/edit"><i class="fa fa-circle-o"></i>カレッジ編集</a></li>
+				<li><a href="/college/add"><i class="fa fa-circle-o"></i>カレッジ追加</a></li>
+			</ul>
+		</li>
 	</ul>
 </section>
 <!-- /.sidebar -->
