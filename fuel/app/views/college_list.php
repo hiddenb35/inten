9c031b4adb189f38a4825b34fd799e9f35f75cc1
Shota Narumi <k013c1112@it-neec.jp>
2015-09-22 01:54:06 +0900

    issue#23 college_list.phpの編集

--- fuel/app/views/college_list.php
+++ fuel/app/views/college_list.php
@@ -1,25 +1,29 @@
-<div id="college_list">
-    <div class="table-responsive">
-        <table class="table table-striped table-hover" style="border :1px #000000;">
-            <thead>
-                <tr>
-                    <th>カレッジ名</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>ITカレッジ</td>
-                </tr>
-                <tr>
-                    <td>クリエイターズカレッジ</td>
-                </tr>
-                <tr>
-                    <td>ミュージックカレッジ</td>
-                </tr>
-                <tr>
-                    <td>テクノロジーカレッジ</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+<section class="content-header">
+    <h1>
+        カレッジ一覧
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    </ol>
+</section>
+<!-- Main content -->
+<section class="content">
+<div id="COLLEGE_LIST">
+    <form class="form-horizontal" method="get">
+        <div class="form-group">
+            <label for="name">カレッジ</label>
+            <!-- .selectpickerを適用するにはファイルをダウンロードしてくる必要あり -->
+            <select class="selectpicker" data-style="btn-primary" name="name" id="name">
+                <option>1</option>
+                <option>1</option>
+                <option>1</option>
+                <option>1</option>
+                <option>1</option>
+                <option>1</option>
+                <option>1</option>
+                <option>1</option>
+            </select>
+        </div>
+    </form>
 </div>
+</section>
