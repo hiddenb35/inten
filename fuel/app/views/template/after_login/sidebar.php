42235989016709ebcc67f26a0a34c3763682e2b3
Keita Kasai <k013c1345@it-neec.jp>
2015-09-22 09:10:54 +0000

    issue#79 サイドバーに生徒関係のメニューを追加しました。

--- fuel/app/views/template/after_login/sidebar.php
+++ fuel/app/views/template/after_login/sidebar.php
@@ -47,6 +47,16 @@
 				<li><a href="/teacher/add"><i class="fa fa-circle-o"></i>教員追加</a></li>
 			</ul>
 		</li>
+		<li class="treeview">
+			<a href="/">
+				<i class="fa ion-university"></i> <span>生徒情報</span> <i class="fa fa-angle-down pull-right"></i>
+			</a>
+			<ul class="treeview-menu">
+				<li><a href="/student/list"><i class="fa fa-circle-o"></i>生徒一覧</a></li>
+				<li><a href="/student/edit"><i class="fa fa-circle-o"></i>生徒編集</a></li>
+				<li><a href="/student/add"><i class="fa fa-circle-o"></i>生徒追加</a></li>
+			</ul>
+		</li>
 		<li>
 			<a href="/lesson/add">
 				<i class="fa ion-university"></i> <span>授業追加</span>
