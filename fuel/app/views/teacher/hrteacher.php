c6ae52d73388d56b6663a6420338bd9413f26e88
Keita Kasai <k013c1345@it-neec.jp>
2015-09-24 10:26:41 +0000

    issue#96 該当ファイルのタブをハードタブに変更しました。

--- fuel/app/views/teacher/hrteacher.php
+++ fuel/app/views/teacher/hrteacher.php
@@ -1,49 +1,49 @@
-<section class="content-header">
-    <h1>
-        担任割り当て
-    </h1>
-    <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-    </ol>
-</section>
-<!-- Main content -->
-<section class="content">
 <div id="HRTEACHER">
-    <form class="form-horizontal" method="post">
-        <div class="form-group">
-            <label class="control-label" for="teacher_id">教員</label>
-            <div class="controls">
-                <div>
-                    <select name="teacher_id">
-                        <option value="0">1</option><!-- 芦沢ゾーン -->
-                        <option value="0">1</option><!-- 芦沢ゾーン -->
-                        <option value="0">1</option><!-- 芦沢ゾーン -->
-                        <option value="0">1</option><!-- 芦沢ゾーン -->
-                        <option value="0">1</option><!-- 芦沢ゾーン -->
-                   </select>
-                </div>
-            </div>
-        </div>
+    <section class="content-header">
+		<h1>
+			担任割り当て
+		</h1>
+		<ol class="breadcrumb">
+			<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+		</ol>
+	</section>
+	<!-- Main content -->
+	<section class="content">
+		<form class="form-horizontal" method="post">
+			<div class="form-group">
+				<label class="control-label" for="teacher_id">教員</label>
+				<div class="controls">
+					<div>
+						<select name="teacher_id">
+							<option value="0">1</option><!-- 芦沢ゾーン -->
+							<option value="0">1</option><!-- 芦沢ゾーン -->
+							<option value="0">1</option><!-- 芦沢ゾーン -->
+							<option value="0">1</option><!-- 芦沢ゾーン -->
+							<option value="0">1</option><!-- 芦沢ゾーン -->
+					   </select>
+					</div>
+				</div>
+			</div>
 
-        <div class="form-group">
-            <label class="control-label" for="name">クラス名</label>
-            <div class="controls">
-                <div>
-                    <select name="name">
-                        <option value="0">IS07-1</option><!-- 芦沢ゾーン -->
-                        <option value="0">IS07-1</option><!-- 芦沢ゾーン -->
-                        <option value="0">IS07-1</option><!-- 芦沢ゾーン -->
-                        <option value="0">IS07-1</option><!-- 芦沢ゾーン -->
-                        <option value="0">IS07-1</option><!-- 芦沢ゾーン -->
-                   </select>
-                </div>
-            </div>
-        </div>
+			<div class="form-group">
+				<label class="control-label" for="name">クラス名</label>
+				<div class="controls">
+					<div>
+						<select name="name">
+							<option value="0">IS07-1</option><!-- 芦沢ゾーン -->
+							<option value="0">IS07-1</option><!-- 芦沢ゾーン -->
+							<option value="0">IS07-1</option><!-- 芦沢ゾーン -->
+							<option value="0">IS07-1</option><!-- 芦沢ゾーン -->
+							<option value="0">IS07-1</option><!-- 芦沢ゾーン -->
+					   </select>
+					</div>
+				</div>
+			</div>
 
 
-        <button type="submit" class="btn btn-primary">登録</button>
-        <button type="reset" class="btn btn-warning">キャンセル</button>
+			<button type="submit" class="btn btn-primary">登録</button>
+			<button type="reset" class="btn btn-warning">キャンセル</button>
 
-    </form>
+		</form>
+	</section>
 </div>
-</section>
