62915a7330e47accd2aa0d76767a6ccd035a0e3f
k013c1118 <k013c1118@it-neec.jp>
2015-09-24 17:21:55 +0900

    #issue68  majorモデルの作成

---
+++ fuel/app/classes/model/major.php
@@ -0,0 +1,36 @@
+<?php
+
+Class Model_Major extends \Orm\Model
+{
+	protected static $_table_name = 'major';
+	protected static $_primary_key = array('id');
+
+	protected static $_properties = array(
+		'id',
+		'name' => array(
+			'data_type' => 'varchar',
+		),
+		'course_id' => array(
+			'data_type' => 'int',
+		),
+		'created_at' => array(
+			'data_type' => 'int',
+		),
+		'updated_at' => array(
+			'data_type' => 'int',
+		),
+	);
+
+	/*	protected static $_observers = array(
+		'Orm\Observer_CreatedAt' => array(
+			'events' => array('before_insert'),
+			'mysql_timestamp' => false,
+			'property' => 'created_at',
+		),
+		'Orm\Observer_UpdatedAt' => array(
+			'events' => array('before_update'),
+			'mysql_timestamp' => false,
+			'property' => 'updated_at',
+		),
+	);*/
+}
