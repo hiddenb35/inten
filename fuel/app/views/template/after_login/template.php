49677301be5ed4294c53a5e556bc60f6cba27f66
Keita Kasai <k013c1345@it-neec.jp>
2015-09-24 08:21:35 +0000

    issue#93 narumi.cssをafter-login.cssにリネームしました。

--- fuel/app/views/template/after_login/template.php
+++ fuel/app/views/template/after_login/template.php
@@ -35,7 +35,7 @@
 		<?php echo Asset::css('bootstrap-select.min.css'); ?>
 
 		<!--  -->
-		<?php echo Asset::css('narumi.css'); ?>
+		<?php echo Asset::css('after-login.css'); ?>
 
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
