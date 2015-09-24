bcf0b9d254496b9d5599d6d5d68d80ca87f8c029
Shota Narumi <k013c1112@it-neec.jp>
2015-09-24 16:41:15 +0900

    issue#70 sutudent_login.phpの修正
    ・全変更
    ・AdminLTEからコピペ

--- fuel/app/views/auth/student_login.php
+++ fuel/app/views/auth/student_login.php
@@ -1,30 +1,39 @@
-<section class="content-header">
-    <h1>
-        ログイン
-    </h1>
-    <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-    </ol>
-</section>
-<!-- Main content -->
-<section class="content">
-<div id="login">
-    <form class="form-horizontal" method="post">
-        <div class="form-group">
-            <div class="input-prepend">
-                <span class="add-on"><i class="ion-social-github"></i></span>
-                <input type="text" class="input-xlarge" id="user" name="username" placeholder="ユーザ名">
-            </div>                    
-        </div>
-        <div class="form-group">
-            <div class="input-prepend">
-                <span class="add-on"><i class="ion-android-lock"></i></span>
-                <input type="password" class="input-xlarge" id="pass" name="password" placeholder="パスワード">
-            </div>
-        </div>
+<div class="login-box">
+      <div class="login-logo">
+        <a href="../../index2.html"><b>ログイン</b></a>
+      </div><!-- /.login-logo -->
+      <div class="login-box-body">
+        <form action="../../index2.html" method="post">
+          <div class="form-group has-feedback">
+            <input type="email" class="form-control" placeholder="Email">
+            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+          </div>
+          <div class="form-group has-feedback">
+            <input type="password" class="form-control" placeholder="Password">
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+          </div>
+          <div class="row">
+            <div class="col-xs-8">
+              <div class="checkbox icheck">
+                <label class="">
+                  <div class="icheckbox_square-blue" aria-checked="false" aria-disabled="false" style="position: relative;"><input type="checkbox" style="position: absolute; top: -20%; left: -20%; display: block; width: 140%; height: 140%; margin: 0px; padding: 0px; border: 0px; opacity: 0; background: rgb(255, 255, 255);"><ins class="iCheck-helper" style="position: absolute; top: -20%; left: -20%; display: block; width: 140%; height: 140%; margin: 0px; padding: 0px; border: 0px; opacity: 0; background: rgb(255, 255, 255);"></ins></div> Remember Me
+                </label>
+              </div>
+            </div><!-- /.col -->
+            <div class="col-xs-4">
+              <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+            </div><!-- /.col -->
+          </div>
+        </form>
 
-        <button type="submit" class="btn btn-primary">ログイン</button>
- 
-    </form>
-</div>
-</section>
+        <div class="social-auth-links text-center">
+          <p>- OR -</p>
+          <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using Facebook</a>
+          <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using Google+</a>
+        </div><!-- /.social-auth-links -->
+
+        <a href="#">I forgot my password</a><br>
+        <a href="register.html" class="text-center">Register a new membership</a>
+
+      </div><!-- /.login-box-body -->
+    </div>
