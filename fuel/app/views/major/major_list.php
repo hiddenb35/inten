59db634c35b8e30b4d0a1e2098694096036187ed
TKato <k013c1140@it-neec.jp>
2015-09-25 21:18:44 +0900

    issue#103 専攻一覧画面の作成をしました。
    項目に設立可能人数を入れてみました。
    あと、テーブルのシマシマをなくしてhovarのみにしてみました。

---
+++ fuel/app/views/major/major_list.php
@@ -0,0 +1,73 @@
+<section class="content-header">
+    <h1>
+        専攻一覧
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    </ol>
+</section>
+<!-- Main content -->
+<section class="content">
+<div id="CLASS_LIST">
+	<div class="box">
+        <div class="box-header with-border">
+            <h3 class="box-title">専攻一覧</h3>
+        </div><!-- /.box-header -->
+        <div class="box-body">
+            <table class="table table-bordered table-hover">
+                <tbody>
+                    <tr>
+                        <th>#</th>
+                        <th>専攻名</th>
+                        <th>設立可能人数</th>
+                        <th>所属学科名</th>
+                        <th>所属カレッジ名</th>
+                    </tr>
+                    <tr>
+                        <td>1.</td>
+                        <td>システム専攻</td>
+                        <td>7名</td>
+                        <td>ITスペシャリスト科</td>
+                        <td>ITカレッジ</td>
+                    </tr>
+                    <tr>
+                        <td>2.</td>
+                        <td>web専攻</td>
+                        <td>7名</td>
+                        <td>ITスペシャリスト科</td>
+                        <td>ITカレッジ</td>
+                    </tr>
+                    <tr>
+                        <td>3.</td>
+                        <td>ネットワーク専攻</td>
+                        <td>7名</td>
+                        <td>ネットワーク科</td>
+                        <td>ITカレッジ</td>
+                    </tr>
+                    <tr>
+                        <td>4.</td>
+                        <td>ネットワーク専攻</td>
+                        <td>7名</td>
+                        <td>ネットワーク科</td>
+                        <td>ITカレッジ</td>
+                    </tr>
+                    <tr>
+                        <td>5.</td>
+                        <td>ネットワーク専攻</td>
+                        <td>7名</td>
+                        <td>ネットワーク科</td>
+                        <td>ITカレッジ</td>
+                    </tr>
+                    <tr>
+                        <td>6.</td>
+                        <td>ネットワーク専攻</td>
+                        <td>7名</td>
+                        <td>ネットワーク科</td>
+                        <td>ITカレッジ</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div><!-- /.box-body -->
+    </div>
+</div>
+</section>
