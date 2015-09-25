fa4f22b11d300e8e3e4c855fa6ae10f39fae419c
Shota Narumi <k013c1112@it-neec.jp>
2015-09-25 15:43:52 +0900

    issue#70 生徒用ログインページの完成

--- fuel/app/views/auth/student_login.php
+++ fuel/app/views/auth/student_login.php
@@ -1,24 +1,28 @@
 <div id="LOGIN">
-<div class="login-box">
-    <div class="login-logo" id="logo">
-        <b>Login</b>
-    </div><!-- /.login-logo -->
-    <div class="login-box-body">
-        <form method="post">
-            <div class="form-group has-feedback">
-                <input type="text" class="form-control input-lg" placeholder="Username">
-                <span class="glyphicon glyphicon-user form-control-feedback"></span>
-            </div>
-            <div class="form-group has-feedback">
-                <input type="password" class="form-control input-lg" placeholder="Password">
-                <span class="ion-unlocked form-control-feedback"></span>
-            </div>
-            <div class="row">
-                <button type="submit" class="btn btn-primary btn-block btn-flat btn-lg">ログイン
-                    <span class="glyphicon glyphicon-log-in"></span>
-                </button>
-            </div>
-        </form>
+<div class="container">
+    <div class="login-box">
+        <div class="login-logo" id="logo">
+            <b>Login</b>
+        </div><!-- /.login-logo -->
+        <div class="login-box-body">
+            <form id="login_form" method="post">
+                <div class="form-group has-feedback">
+                    <input type="text" class="form-control input-lg" placeholder="Username">
+                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                </div>
+                <div class="form-group has-feedback">
+                    <input type="password" class="form-control input-lg" placeholder="Password">
+                    <span class="ion-unlocked form-control-feedback"></span>
+                </div>
+<!--                 <div class="row">
+                    <div class="col-xs-6">
+ -->                        <button type="submit" class="btn btn-block btn-primary btn-flat btn-lg center-block">Login
+                            <span class="glyphicon glyphicon-log-in"></span>
+                        </button>
+<!--                     </div>
+ -->                </div>
+            </form>
+        </div>
     </div>
 </div>
 </div>
