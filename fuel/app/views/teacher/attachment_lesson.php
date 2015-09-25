1e63c765f824fa5cfd4b64fdd80ace2116f36d89
Keita Kasai <k013c1345@it-neec.jp>
2015-09-25 03:08:09 +0000

    issue#96 授業割り当てページのデザインを変更しました。

--- fuel/app/views/teacher/attachment_lesson.php
+++ fuel/app/views/teacher/attachment_lesson.php
@@ -12,28 +12,32 @@
 		<div class="box box-warning">
 			<div class="box-body">
 				<form method="post" role="form">
-					<div class="form-group">
-						<label for="teacher_id">教員</label>
-						<select id="teacher_id" name="teacher_id" class="form-control">
-							<option value="1">教員太郎</option><!-- 芦沢ゾーン -->
-							<option value="2">担任太郎</option><!-- 芦沢ゾーン -->
-							<option value="3">AdminAdmin</option><!-- 芦沢ゾーン -->
-						</select>
-					</div>
+					<div class="row">
+						<div class="form-group col-sm-4">
+							<label for="teacher_id">教員</label>
+							<select id="teacher_id" name="teacher_id" class="form-control">
+								<option value="1">教員太郎</option><!-- 芦沢ゾーン -->
+								<option value="2">担任太郎</option><!-- 芦沢ゾーン -->
+								<option value="3">AdminAdmin</option><!-- 芦沢ゾーン -->
+							</select>
+						</div>
 
-					<div class="form-group">
-						<label for="lesson_id">授業</label>
-						<select id="lesson_id" name="lesson_id" class="form-control">
-							<option value="1">資格対策講座</option><!-- 芦沢ゾーン -->
-							<option value="2">C言語プログラミング</option><!-- 芦沢ゾーン -->
-							<option value="3">経営科学</option><!-- 芦沢ゾーン -->
-							<option value="4">Javaプログラミング</option><!-- 芦沢ゾーン -->
-							<option value="5">Linux実習</option><!-- 芦沢ゾーン -->
-						</select>
-					</div>
+						<div class="form-group col-sm-4">
+							<label for="lesson_id">授業</label>
+							<select id="lesson_id" name="lesson_id" class="form-control">
+								<option value="1">資格対策講座</option><!-- 芦沢ゾーン -->
+								<option value="2">C言語プログラミング</option><!-- 芦沢ゾーン -->
+								<option value="3">経営科学</option><!-- 芦沢ゾーン -->
+								<option value="4">Javaプログラミング</option><!-- 芦沢ゾーン -->
+								<option value="5">Linux実習</option><!-- 芦沢ゾーン -->
+							</select>
+						</div>
 
-					<button type="submit" class="btn btn-primary">登録</button>
-					<button type="reset" class="btn btn-warning">キャンセル</button>
+						<div class="col-sm-4">
+							<button type="submit" class="btn btn-primary">登録</button>
+							<button type="reset" class="btn btn-warning">キャンセル</button>
+						</div>
+					</div>
 
 				</form>
 			</div>
