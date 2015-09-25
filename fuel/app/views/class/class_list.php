3d2d406de233158d0d48267b5a6117ac6c9d7d56
TKato <k013c1140@it-neec.jp>
2015-09-25 19:38:22 +0900

    issue#102 クラス一覧画面の作成
    クラス一覧画面の作成をしました。
    見た目が寂しかったのでデータに担任を追加してみました。
    ないほうがいいかもしれません。

---
+++ fuel/app/views/class/class_list.php
@@ -0,0 +1,69 @@
+<section class="content-header">
+    <h1>
+        クラス一覧
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    </ol>
+</section>
+<!-- Main content -->
+<section class="content">
+<div id="CLASS_LIST">
+	<div class="box">
+        <div class="box-header with-border">
+            <h3 class="box-title">クラス一覧</h3>
+        </div><!-- /.box-header -->
+        <div class="box-body">
+            <table class="table table-bordered table-striped table-hover">
+                <tbody>
+                    <tr>
+                        <th>#</th>
+                        <th>クラス名</th>
+                        <th>担任名</th>
+                        <th>所属学科名</th>
+                        <th>所属カレッジ名</th>
+                    </tr>
+                    <tr>
+                        <td>1.</td>
+                        <td>IS07-1</td>
+                        <td>黛さん</td>
+                        <td>ITスペシャリスト科</td>
+                        <td>ITカレッジ</td>
+                    </tr>
+                    <tr>
+                        <td>2.</td>
+                        <td>IS07-1</td>
+                        <td>黛さん</td>
+                        <td>ITスペシャリスト科</td>
+                        <td>ITカレッジ</td>
+                    </tr><tr>
+                        <td>3.</td>
+                        <td>IS07-1</td>
+                        <td>黛さん</td>
+                        <td>ITスペシャリスト科</td>
+                        <td>ITカレッジ</td>
+                    </tr><tr>
+                        <td>4.</td>
+                        <td>IS07-1</td>
+                        <td>黛さん</td>
+                        <td>ITスペシャリスト科</td>
+                        <td>ITカレッジ</td>
+                    </tr><tr>
+                        <td>5.</td>
+                        <td>IS07-1</td>
+                        <td>黛さん</td>
+                        <td>ITスペシャリスト科</td>
+                        <td>ITカレッジ</td>
+                    </tr><tr>
+                        <td>6.</td>
+                        <td>IS07-1</td>
+                        <td>黛さん</td>
+                        <td>ITスペシャリスト科</td>
+                        <td>ITカレッジ</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div><!-- /.box-body -->
+    </div>
+</div>
+</section>
