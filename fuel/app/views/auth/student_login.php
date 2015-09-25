6d539e4fb2fcffb7ef23a91fee1969eac042e957
Shota Narumi <k013c1112@it-neec.jp>
2015-09-25 14:13:09 +0900

    issue#70 vagrant再構築のためのコミット

--- fuel/app/views/auth/student_login.php
+++ fuel/app/views/auth/student_login.php
@@ -1,31 +1,24 @@
 <div id="LOGIN">
 <div class="login-box">
-      <div class="login-logo" id="logo">
+    <div class="login-logo" id="logo">
         <b>Login</b>
-      </div><!-- /.login-logo -->
-      <div class="login-box-body">
+    </div><!-- /.login-logo -->
+    <div class="login-box-body">
         <form method="post">
-          <div class="form-group has-feedback">
-            <input type="text" class="form-control" placeholder="Username">
-            <span class="glyphicon glyphicon-user form-control-feedback"></span>
-          </div>
-          <div class="form-group has-feedback">
-            <input type="password" class="form-control" placeholder="Password">
-            <span class="ion-unlocked form-control-feedback"></span>
-          </div>
-          <div class="row">
-            <div class="col-xs-8">
-              <div class="checkbox icheck">
-                <label class="control-label">
-                  <div class="icheckbox_square-blue" aria-checked="false" aria-disabled="false"><input type="checkbox"><ins class="iCheck-helper"></ins></div>
-                </label>
-              </div>
-            </div><!-- /.col -->
-<!--             <div class="col-xs-4">
- -->              <button type="submit" class="btn btn-primary btn-block btn-flat">ログイン</button>
-<!--             </div> --><!-- /.col -->
-          </div>
+            <div class="form-group has-feedback">
+                <input type="text" class="form-control input-lg" placeholder="Username">
+                <span class="glyphicon glyphicon-user form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+                <input type="password" class="form-control input-lg" placeholder="Password">
+                <span class="ion-unlocked form-control-feedback"></span>
+            </div>
+            <div class="row">
+                <button type="submit" class="btn btn-primary btn-block btn-flat btn-lg">ログイン
+                    <span class="glyphicon glyphicon-log-in"></span>
+                </button>
+            </div>
         </form>
-    </div><!-- /.social-auth-links -->
-</div><!-- /.login-box-body -->
+    </div>
+</div>
 </div>
