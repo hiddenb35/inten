28cd83b6ec5f5a02a4a971e26053b2603eb45ad2
Keita Kasai <k013c1345@it-neec.jp>
2015-09-26 08:25:45 +0000

    issue118 ハードタブに変更しました。

--- fuel/app/views/teacher/assign_list.php
+++ fuel/app/views/teacher/assign_list.php
@@ -1,65 +1,65 @@
 <div id="ASSIGN_LIST">
-    <section class="content-header">
-        <h1>
-            クラス一覧
-        </h1>
-        <ol class="breadcrumb">
-            <li><a href="#">Home > クラス一覧</a></li>
-        </ol>
-    </section>
-    <!-- Main content -->
-    <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-6 col-md-3">
-                    <div class="box box-info text-center">
-                        <a href="#">
-                            <div class="box-body">
-                                <div class="class_name h1">IS07-1</div>
-                                <div class="college_name">ITカレッジ</div>
-                                <div class="course_name">ITスペシャリスト科</div>
-                                <div class="class_people">40名</div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-md-3">
-                    <div class="box box-info text-center">
-                        <a href="#">
-                            <div class="box-body">
-                                <div class="class_name h1">IS07-2</div>
-                                <div class="college_name">ITカレッジ</div>
-                                <div class="course_name">ITスペシャリスト科</div>
-                                <div class="class_people">40名</div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-md-3">
-                    <div class="box box-info text-center">
-                        <a href="#">
-                            <div class="box-body">
-                                <div class="class_name h1">IS08-1</div>
-                                <div class="college_name">ITカレッジ</div>
-                                <div class="course_name">ITスペシャリスト科</div>
-                                <div class="class_people">40名</div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-md-3">
-                    <div class="box box-info text-center">
-                        <a href="#">
-                            <div class="box-body">
-                                <div class="class_name h1">IS08-2</div>
-                                <div class="college_name">ITカレッジ</div>
-                                <div class="course_name">ITスペシャリスト科</div>
-                                <div class="class_people">40名</div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+	<section class="content-header">
+		<h1>
+			クラス一覧
+		</h1>
+		<ol class="breadcrumb">
+			<li><a href="#">Home > クラス一覧</a></li>
+		</ol>
+	</section>
+	<!-- Main content -->
+	<section class="content">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm-6 col-md-3">
+					<div class="box box-info text-center">
+						<a href="#">
+							<div class="box-body">
+								<div class="class_name h1">IS07-1</div>
+								<div class="college_name">ITカレッジ</div>
+								<div class="course_name">ITスペシャリスト科</div>
+								<div class="class_people">40名</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="box box-info text-center">
+						<a href="#">
+							<div class="box-body">
+								<div class="class_name h1">IS07-2</div>
+								<div class="college_name">ITカレッジ</div>
+								<div class="course_name">ITスペシャリスト科</div>
+								<div class="class_people">40名</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="box box-info text-center">
+						<a href="#">
+							<div class="box-body">
+								<div class="class_name h1">IS08-1</div>
+								<div class="college_name">ITカレッジ</div>
+								<div class="course_name">ITスペシャリスト科</div>
+								<div class="class_people">40名</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="box box-info text-center">
+						<a href="#">
+							<div class="box-body">
+								<div class="class_name h1">IS08-2</div>
+								<div class="college_name">ITカレッジ</div>
+								<div class="course_name">ITスペシャリスト科</div>
+								<div class="class_people">40名</div>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 </div>
