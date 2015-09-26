aa728208a87ecae3703a93ea246275774e94610f
Keita Kasai <k013c1345@it-neec.jp>
2015-09-26 04:32:00 +0000

    issue#114 カレッジ一覧にカレッジ追加フォームを追加しました。

--- fuel/app/views/college/college_list.php
+++ fuel/app/views/college/college_list.php
@@ -69,5 +69,26 @@
 				</table>
 			</div><!-- /.box-body -->
 		</div>
+		<div class="box">
+			<div class="box-header with-border">
+				<h3 class="box-title">カレッジ追加</h3>
+			</div><!-- /.box-header -->
+			<div class="box-body">
+                <form action="/teacher/hrteacher" method="post" role="form" class="form-horizontal">
+                    <div class="row">
+                        <div class="form-group" id="form_college_add">
+                            <label for="name" class="col-sm-1 control-label">カレッジ名</label>
+                            <div class="col-sm-3">
+                                <input type="text" class="form-control" id="name" name="name">
+                            </div>
+                            <div class="form-button col-sm-8">
+                                <button type="submit" class="btn btn-primary">登録</button>
+                                <button type="reset" class="btn btn-warning">キャンセル</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+			</div><!-- /.box-body -->
+		</div>
 	</section>
 </div>
