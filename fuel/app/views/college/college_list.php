04e5414819d74cef2fcebf120d4b4010f55fd0e6
Keita Kasai <k013c1345@it-neec.jp>
2015-09-26 03:44:46 +0000

    issue#114 ハードタブに変更しました。

--- fuel/app/views/college/college_list.php
+++ fuel/app/views/college/college_list.php
@@ -1,73 +1,73 @@
-<section class="content-header">
-    <h1>
-        カレッジ一覧
-    </h1>
-    <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-    </ol>
-</section>
-<!-- Main content -->
-<section class="content">
 <div id="COLLEGE_LIST">
-	<div class="box">
-        <div class="box-header with-border">
-            <h3 class="box-title">カレッジ一覧</h3>
-        </div><!-- /.box-header -->
-        <div class="box-body">
-            <table class="table table-bordered table-striped table-hover">
-                <tbody>
-                    <tr>
-                        <th>#</th>
-                        <th>カレッジ名</th>
-                        <th>学科数</th>
-                        <th>クラス数</th>
-                        <th>専攻数</th>
-                    </tr>
-                    <tr>
-                        <td>1.</td>
-                        <td>ITカレッジ</td>
-                        <td>3</td>
-                        <td>10</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>2.</td>
-                        <td>クリエイターズカレッジ</td>
-                        <td>3</td>
-                        <td>10</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>3.</td>
-                        <td>ミュージックカレッジ</td>
-                        <td>3</td>
-                        <td>10</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>4.</td>
-                        <td>テクノロジーカレッジ</td>
-                        <td>3</td>
-                        <td>10</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>5.</td>
-                        <td>デザインカレッジ</td>
-                        <td>3</td>
-                        <td>10</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>6.</td>
-                        <td>医療カレッジ</td>
-                        <td>3</td>
-                        <td>10</td>
-                        <td>2</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div><!-- /.box-body -->
-    </div>
+	<section class="content-header">
+		<h1>
+			カレッジ一覧
+		</h1>
+		<ol class="breadcrumb">
+			<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+		</ol>
+	</section>
+	<!-- Main content -->
+	<section class="content">
+		<div class="box">
+			<div class="box-header with-border">
+				<h3 class="box-title">カレッジ一覧</h3>
+			</div><!-- /.box-header -->
+			<div class="box-body">
+				<table class="table table-bordered table-striped table-hover">
+					<tbody>
+						<tr>
+							<th>#</th>
+							<th>カレッジ名</th>
+							<th>学科数</th>
+							<th>クラス数</th>
+							<th>専攻数</th>
+						</tr>
+						<tr>
+							<td>1.</td>
+							<td>ITカレッジ</td>
+							<td>3</td>
+							<td>10</td>
+							<td>2</td>
+						</tr>
+						<tr>
+							<td>2.</td>
+							<td>クリエイターズカレッジ</td>
+							<td>3</td>
+							<td>10</td>
+							<td>2</td>
+						</tr>
+						<tr>
+							<td>3.</td>
+							<td>ミュージックカレッジ</td>
+							<td>3</td>
+							<td>10</td>
+							<td>2</td>
+						</tr>
+						<tr>
+							<td>4.</td>
+							<td>テクノロジーカレッジ</td>
+							<td>3</td>
+							<td>10</td>
+							<td>2</td>
+						</tr>
+						<tr>
+							<td>5.</td>
+							<td>デザインカレッジ</td>
+							<td>3</td>
+							<td>10</td>
+							<td>2</td>
+						</tr>
+						<tr>
+							<td>6.</td>
+							<td>医療カレッジ</td>
+							<td>3</td>
+							<td>10</td>
+							<td>2</td>
+						</tr>
+					</tbody>
+				</table>
+			</div><!-- /.box-body -->
+		</div>
+	</section>
 </div>
-</section>
