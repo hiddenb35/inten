94dbabe5d22b14cd87f1f8adb2b538d5aefb1cf9
Shota Narumi <k013c1112@it-neec.jp>
2015-09-26 12:15:13 +0900

    issue#101 パスワード変更画面の修正
    ・bootstrapの適用

--- fuel/app/views/auth/pass_update.php
+++ fuel/app/views/auth/pass_update.php
@@ -1,40 +1,64 @@
+<div id="PASS_UPDATE">
 <section class="content-header">
     <h1>
         パスワード変更
     </h1>
     <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Home > パスワード変更</a></li>
     </ol>
 </section>
 <!-- Main content -->
 <section class="content">
-<div id="pass_update">
-    <form class="form-horizontal" method="post">
-        <div class="form-group">
-            <label for="current_pass" class="control-label">現在のパスワード</label>
-            <div class="controls">
-                <input type="password" class="input-xlarge" id="current_pass" name="current_password">
-            </div>
-        </div>
+    <div class="container">
+        <form class="form-horizontal" method="post">
 
-        <div class="form-group">
-            <label for="new_pass" class="control-label">新規パスワード</label>
-            <div class="controls">
-                <input type="password" class="input-xlarge" id="new_pass" name="new_password">
+            <div class="form-group form-group-lg">
+                <label for="current_pass" class="control-label col-sm-2">現在のパスワード</label>
+                <div class="col-sm-10">
+                    <div class="input-group col-sm-8">
+                        <span class="input-group-addon">
+                            <i class="ion-unlocked"></i>
+                        </span>
+                        <input type="password" class="form-control" id="current_pass" name="current_password">
+                    </div>
+                </div>
             </div>
-        </div>
 
-        <div class="form-group">
-            <label for="check_pass" class="control-label">新規パスワード(確認)</label>
-            <div class="controls">
-                <input type="password" class="input-xlarge" id="check_pass" name="new_password_confirm">
+            <div class="form-group form-group-lg">
+                <label for="new_pass" class="control-label col-sm-2">新規パスワード</label>
+                <div class="col-sm-10">
+                    <div class="input-group col-sm-8">
+                        <span class="input-group-addon">
+                            <i class="ion-locked"></i>
+                        </span>
+                        <input type="password" class="form-control" id="new_pass" name="new_password">
+                    </div>
+                </div>
             </div>
-        </div>
 
-        <div class="form-group">
-            <button type="submit" class="btn btn-primary">変更</button>
-            <button type="reset" class="btn btn-warning">キャンセル</button>
-        </div>
-    </form>
-</div>
+            <div class="form-group form-group-lg">
+                <label for="check_pass" class="control-label col-sm-2">新規パスワード(確認)</label>
+                <div class="col-sm-10">
+                    <div class="input-group col-sm-8">
+                        <span class="input-group-addon">
+                            <i class="ion-locked"></i>
+                        </span>
+                        <input type="password" class="form-control" id="check_pass" name="new_password_confirm">
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-sm-10">
+                    <div class="row">
+                        <div class="col-sm-12 text-center">
+                            <button type="submit" class="btn btn-primary btn-lg">変更</button>
+                            <button type="reset" class="btn btn-warning btn-lg">キャンセル</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
 </section>
+</div>
