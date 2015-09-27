bc225dcc5910c5fbf7180be7aab4a7708b3a37e6
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-09-28 03:03:19 +0900

    issue#154 出席画面でデータを動的表示、JSを拡張

--- fuel/app/views/attendance/attendance.php
+++ fuel/app/views/attendance/attendance.php
@@ -10,63 +10,29 @@
 <section class="content">
 	<div id="ATTENDANCE">
 		<form action="/attendance" method="post" class="container-fluid">
-			<div class="form-group col-sm-4 col-md-3">
-				<input type="hidden" name="attendance[0][student_id]" value="1">
-				<div class="box box-success">
-					<div class="box-header with-border">
-						<h3 class="box-title">k013c1129 渡辺優樹</h3>
-						<div class="box-tools pull-right">
-							<button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+			<input type="hidden" name="class_id" value="<?php echo $class_id; ?>">
+			<?php foreach($student_lists as $index => $student): ?>
+				<div class="form-group col-sm-4 col-md-3">
+					<input type="hidden" name="attendance[<?php echo $index; ?>][student_id]" value="<?php echo $student['id']; ?>">
+					<div class="box box-success">
+						<div class="box-header with-border">
+							<h3 class="box-title"><?php echo $student['number']; ?> <?php echo $student['full_name']; ?></h3>
+							<div class="box-tools pull-right">
+								<b class="attendance-status"></b>
+								<button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+							</div>
 						</div>
-					</div>
-					<div class="box-body">
-						<div data-toggle="buttons" class="text-center">
-							<label class="btn btn-default btn-lg"><input type="radio" name="attendance[0][status]" value="1">出席</label>
-							<label class="btn btn-default btn-lg"><input type="radio" name="attendance[0][status]" value="2">遅刻</label>
-							<label class="btn btn-default btn-lg"><input type="radio" name="attendance[0][status]" value="3">欠席</label>
-							<label class="btn btn-default btn-lg"><input type="radio" name="attendance[0][status]" value="4">公欠</label>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="form-group col-sm-4 col-md-3">
-				<input type="hidden" name="attendance[1][student_id]" value="2">
-				<div class="box box-success">
-					<div class="box-header with-border">
-						<h3 class="box-title">k013c1130 渡辺優樹</h3>
-						<div class="box-tools pull-right">
-							<button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-						</div>
-					</div>
-					<div class="box-body">
-						<div data-toggle="buttons" class="text-center">
-							<label class="btn btn-default btn-lg"><input type="radio" name="attendance[1][status]" value="1">出席</label>
-							<label class="btn btn-default btn-lg"><input type="radio" name="attendance[1][status]" value="2">遅刻</label>
-							<label class="btn btn-default btn-lg"><input type="radio" name="attendance[1][status]" value="3">欠席</label>
-							<label class="btn btn-default btn-lg"><input type="radio" name="attendance[1][status]" value="4">公欠</label>
+						<div class="box-body">
+							<div data-toggle="buttons" class="text-center">
+								<label class="btn btn-default btn-lg" data-attendance-class="text-success"><input type="radio" name="attendance[<?php echo $index; ?>][status]" value="1">出席</label>
+								<label class="btn btn-default btn-lg" data-attendance-class="text-warning"><input type="radio" name="attendance[<?php echo $index; ?>][status]" value="2">遅刻</label>
+								<label class="btn btn-default btn-lg" data-attendance-class="text-danger"><input type="radio" name="attendance[<?php echo $index; ?>][status]" value="3">欠席</label>
+								<label class="btn btn-default btn-lg" data-attendance-class="text-info"><input type="radio" name="attendance[<?php echo $index; ?>][status]" value="4">公欠</label>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-			<div class="form-group col-sm-4 col-md-3">
-				<input type="hidden" name="attendance[2][student_id]" value="3">
-				<div class="box box-success">
-					<div class="box-header with-border">
-						<h3 class="box-title">k013c1131 渡辺優樹</h3>
-						<div class="box-tools pull-right">
-							<button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-						</div>
-					</div>
-					<div class="box-body">
-						<div data-toggle="buttons" class="text-center">
-							<label class="btn btn-default btn-lg"><input type="radio" name="attendance[2][status]" value="1">出席</label>
-							<label class="btn btn-default btn-lg"><input type="radio" name="attendance[2][status]" value="2">遅刻</label>
-							<label class="btn btn-default btn-lg"><input type="radio" name="attendance[2][status]" value="3">欠席</label>
-							<label class="btn btn-default btn-lg"><input type="radio" name="attendance[2][status]" value="4">公欠</label>
-						</div>
-					</div>
-				</div>
-			</div>
+			<?php endforeach; ?>
 			<div class="form-group col-xs-12 text-center">
 				<button type="submit" class="btn btn-primary">送信</button>
 				<button type="reset" class="btn btn-warning">リセット</button>
@@ -77,7 +43,13 @@
 <script>
 	$(function(){
 		$('.box-body label').click(function(){
+			var status = $(this).text();
+			var color = $(this).data('attendance-class');
+			$(this).parents('.box').find('.attendance-status').removeClass(function(index, className) {
+				return (className.match(/\btext-\S+/g) || []).join(' ');
+			}).addClass(color).text(status);
 			$(this).parents('.box').find('button').click();
 		});
+
 	})
 </script>
