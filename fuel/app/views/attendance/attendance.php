023bad04e207a8cd62474ab81c37d5c3ccc34068
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-09-28 03:22:11 +0900

    issue#154 出席画面でのJSを拡張

--- fuel/app/views/attendance/attendance.php
+++ fuel/app/views/attendance/attendance.php
@@ -14,7 +14,7 @@
 			<?php foreach($student_lists as $index => $student): ?>
 				<div class="form-group col-sm-4 col-md-3">
 					<input type="hidden" name="attendance[<?php echo $index; ?>][student_id]" value="<?php echo $student['id']; ?>">
-					<div class="box box-success">
+					<div class="box">
 						<div class="box-header with-border">
 							<h3 class="box-title"><?php echo $student['number']; ?> <?php echo $student['full_name']; ?></h3>
 							<div class="box-tools pull-right">
@@ -24,10 +24,10 @@
 						</div>
 						<div class="box-body">
 							<div data-toggle="buttons" class="text-center">
-								<label class="btn btn-default btn-lg" data-attendance-class="text-success"><input type="radio" name="attendance[<?php echo $index; ?>][status]" value="1">出席</label>
-								<label class="btn btn-default btn-lg" data-attendance-class="text-warning"><input type="radio" name="attendance[<?php echo $index; ?>][status]" value="2">遅刻</label>
-								<label class="btn btn-default btn-lg" data-attendance-class="text-danger"><input type="radio" name="attendance[<?php echo $index; ?>][status]" value="3">欠席</label>
-								<label class="btn btn-default btn-lg" data-attendance-class="text-info"><input type="radio" name="attendance[<?php echo $index; ?>][status]" value="4">公欠</label>
+								<label class="btn btn-default btn-lg" data-box-class="box-success" data-attendance-class="text-success"><input type="radio" name="attendance[<?php echo $index; ?>][status]" value="1">出席</label>
+								<label class="btn btn-default btn-lg" data-box-class="box-warning" data-attendance-class="text-warning"><input type="radio" name="attendance[<?php echo $index; ?>][status]" value="2">遅刻</label>
+								<label class="btn btn-default btn-lg" data-box-class="box-danger" data-attendance-class="text-danger"><input type="radio" name="attendance[<?php echo $index; ?>][status]" value="3">欠席</label>
+								<label class="btn btn-default btn-lg" data-box-class="box-info" data-attendance-class="text-info"><input type="radio" name="attendance[<?php echo $index; ?>][status]" value="4">公欠</label>
 							</div>
 						</div>
 					</div>
@@ -44,11 +44,16 @@
 	$(function(){
 		$('.box-body label').click(function(){
 			var status = $(this).text();
-			var color = $(this).data('attendance-class');
+			var box_color = $(this).data('box-class');
+			var attendance_color = $(this).data('attendance-class');
+
+			$(this).parents('.box').removeClass(function(index, className) {
+				return (className.match(/\bbox-\S+/g) || []).join(' ');
+			});
 			$(this).parents('.box').find('.attendance-status').removeClass(function(index, className) {
 				return (className.match(/\btext-\S+/g) || []).join(' ');
-			}).addClass(color).text(status);
-			$(this).parents('.box').find('button').click();
+			}).addClass(attendance_color).text(status);
+			$(this).parents('.box').addClass(box_color).find('button').click();
 		});
 
 	})
