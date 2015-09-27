677ce7bb8f367d15ad6b5ba1fbd1d33aee9b103f
Keita Kasai <k013c1345@it-neec.jp>
2015-09-27 01:26:00 +0000

    issue#142 学科一覧画面のデータを一番左のカラム以外、動的に表示させるようにしました。

--- fuel/app/views/course/course_list.php
+++ fuel/app/views/course/course_list.php
@@ -1,66 +1,40 @@
 <section class="content-header">
-    <h1>
-        学科一覧
-    </h1>
-    <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-    </ol>
+	<h1>
+		学科一覧
+	</h1>
+	<ol class="breadcrumb">
+		<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+	</ol>
 </section>
 <!-- Main content -->
 <section class="content">
 <div id="COURSE_LIST">
-    <div class="box">
-        <div class="box-header with-border">
-            <h3 class="box-title">学科一覧</h3>
-        </div><!-- /.box-header -->
-        <div class="box-body">
-            <table class="table table-bordered table-striped table-hover">
-                <tbody>
-                    <tr>
-                        <th>#</th>
-                        <th>学科コード</th>
-                        <th>学科名</th>
-                        <th>年制</th>
-                        <th>所属カレッジ名</th>
-                    </tr>
-                    <tr>
-                        <td>1.</td>
-                        <td>IS</td>
-                        <td>ITスペシャリスト科</td>
-                        <td>4</td>
-                        <td>ITカレッジ</td>
-                    </tr>
-                    <tr>
-                        <td>2.</td>
-                        <td>MD</td>
-                        <td>レコーディングクリエイター科</td>
-                        <td>2</td>
-                        <td>ITカレッジ</td>
-                    </tr>
-                    <tr>
-                        <td>3.</td>
-                        <td>U4</td>
-                        <td>建築学科</td>
-                        <td>2</td>
-                        <td>ITカレッジ</td>
-                    </tr>
-                    <tr>
-                        <td>4.</td>
-                        <td>IS</td>
-                        <td>ITスペシャリスト科</td>
-                        <td>4</td>
-                        <td>クリエイターズカレッジ</td>
-                    </tr>
-                    <tr>
-                        <td>5.</td>
-                        <td>IS</td>
-                        <td>ITスペシャリスト科</td>
-                        <td>4</td>
-                        <td>ITカレッジ</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div><!-- /.box-body -->
-    </div>
+	<div class="box">
+		<div class="box-header with-border">
+			<h3 class="box-title">学科一覧</h3>
+		</div><!-- /.box-header -->
+		<div class="box-body">
+			<table class="table table-bordered table-striped table-hover">
+				<tbody>
+					<tr>
+						<th>#</th>
+						<th>学科コード</th>
+						<th>学科名</th>
+						<th>年制</th>
+						<th>所属カレッジ名</th>
+					</tr>
+					<?php foreach($course_lists as $course_list): ?>
+					<tr>
+						<td>1.</td>
+						<td><?php echo $course_list['code']; ?></td>
+						<td><?php echo $course_list['name']; ?></td>
+						<td><?php echo $course_list['year_system']; ?></td>
+						<td><?php echo $course_list['college_name']; ?></td>
+					</tr>
+					<?php endforeach; ?>
+				</tbody>
+			</table>
+		</div><!-- /.box-body -->
+	</div>
 </div>
 </section>
