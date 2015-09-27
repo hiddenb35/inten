5f9b5bc5441a36f1aeb9de18475b64b6510d34d5
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-09-28 03:02:36 +0900

    issue#154 出席画面にデータを送信

--- fuel/app/classes/controller/attendance.php
+++ fuel/app/classes/controller/attendance.php
@@ -4,7 +4,37 @@ class Controller_Attendance extends Controller_Loggedin
 {
 	public function action_index()
 	{
+		$class_id = Input::get('class_id');
+		if(is_null($class_id))
+		{
+			throw new HttpNotFoundException;
+		}
+
 		$this->template->title = '出席';
 		$this->template->content = View::forge('attendance/attendance.php');
+		$this->template->content->set('class_id', $class_id);
+		$this->template->content->set('student_lists', $this->_get_list($class_id));
 	}
+
+	private function _get_list($class_id)
+	{
+		$student_lists = array();
+		$student = Model_Student::find('all', array(
+			'where' => array(
+				array('class_id', '=', $class_id),
+			),
+		));
+
+		foreach($student as $s)
+		{
+			$array = array();
+			$array['id'] = $s['id'];
+			$array['number'] = $s['username'];
+			$array['full_name'] = $s['last_name'] . ' ' . $s['first_name'];
+			$student_lists[] = $array;
+		}
+
+		return $student_lists;
+	}
+
 }
