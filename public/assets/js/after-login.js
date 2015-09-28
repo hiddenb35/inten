43ee0724a84f2e411dad5f81a51c6bc68bb3d1bb
TKato <k013c1140@it-neec.jp>
2015-09-29 00:13:53 +0900

    issue#122 時間割作成画面の編集及び項目を編集
    一応　送るデータを作るところまで終わりました。
    変数名がかぶる可能性あり、jsを書く場所が正しいか？for文の内部に定数が使われてる、cssは書いてないので見にくい。
    などの問題点がありますが、確認のためにコミットしました。

--- public/assets/js/after-login.js
+++ public/assets/js/after-login.js
@@ -2,11 +2,45 @@
  * Created by IT College on 2015/09/26.
  */
 $(function(){
-    $.fn.editable.defaults.mode = 'inline';
-    $('.college-name').editable({
-        type: 'text',
-        pk: 1,
-        url: '#',
-        title: 'Enter username'
-    });
+	$.fn.editable.defaults.mode = 'inline';
+	$('.college-name').editable({
+		type: 'text',
+		pk: 1,
+		url: '#',
+		title: 'Enter username'
+	});
+	var thisStorage ;
+	$("#TIMETABLE_ADD #subject").change(function(){
+		$("#teacher").val($('#subject option:selected').data('teacher'));
+	});
+	$("#TIMETABLE_ADD table td").click(function() {
+		thisStorage = ($(this));
+		$("#TIMETABLE_ADD #timeadd").click();
+		$('#subject').val($(this).children('span').text());
+		$("#teacher").val($('#subject option:selected').data('teacher'));
+		$('#classroom').val($(this).children('.classroom').text());
+		$("#note").val($(this).children('.note').text());
+
+		$("#set").click(function(){
+			$(thisStorage).children('span').text($('#subject option:selected').val());
+			$(thisStorage).children('.subject').text($('#subject option:selected').text());
+			$(thisStorage).children('.teacher').text($('#subject option:selected').data('teacher'));
+			$(thisStorage).children('.classroom').text($('#classroom').val());
+			$(thisStorage).children('.note').text($('#note').val());
+			// console.log();
+		});
+	});
+	var data = {"title":"aaa","params":[]};
+	var tr = $("table tr");
+	$("#TIMETABLE_ADD #transmission").click(function(){
+		for (var i = 1; i < 9; i++) {
+			var cells = tr.eq(i).children();
+			data.params[i-1] = [];
+			for (var j = 1; j < 6; j++) {
+				data.params[i-1][j-1] = {"id":cells.eq(j).children('span').text(),"classroom":cells.eq(j).children('.classroom').text(),"note":cells.eq(j).children('.note').text()};
+			}
+		}
+		data = JSON.stringify(data);
+		console.log(data);
+	});
 });
