4d35722eac250e3a1c01600fbae3a71ad79b6da0
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-09-30 01:04:17 +0900

    issue#165 学科の追加メソッドを実装

--- fuel/app/classes/controller/course.php
+++ fuel/app/classes/controller/course.php
@@ -4,8 +4,28 @@ class Controller_Course extends Controller_Loggedin
 {
 	public function action_add()
 	{
-		$this->template->title = '学科の追加';
-		$this->template->content = View::forge('course/course_add');
+		if(Input::method() !== 'POST')
+		{
+			throw new HttpNotFoundException;
+		}
+		$val = Model_Course::validate();
+		if($val->run())
+		{
+			$course = Model_Course::forge();
+			$course->code = $val->validated('code');
+			$course->name = $val->validated('name');
+			$course->year_system = $val->validated('year_system');
+			$course->college_id = $val->validated('college_id');
+			$course->save();
+
+			Response::redirect('course/list');
+		}
+
+		$this->template->title = 'エラー';
+		$this->template->content = View::forge('course/course_list');
+		$this->template->content->set('course_lists', $this-> _get_list());
+		$this->template->content->set('errors', $val->error_message());
+		$this->template->content->set('inputs', $val->input());
 	}
 
 	public function action_edit()
@@ -18,7 +38,7 @@ class Controller_Course extends Controller_Loggedin
 	{
 		$this->template->title = '学科一覧';
 		$this->template->content = View::forge('course/course_list');
-		$this->template->content->set('course_lists',$this->_get_list());
+		$this->template->content->set('course_lists', $this->_get_list());
 	}
 
 	private function _get_list()
