eebbd299b9f4e8ebda303d9ee4447cd1277a2158
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-09-30 00:40:22 +0900

    issue#165 学科モデルにuniqueバリデーションを追加

--- fuel/app/classes/model/course.php
+++ fuel/app/classes/model/course.php
@@ -71,7 +71,7 @@ class Model_Course extends \Orm\Model
 	{
 		$val = Validation::forge();
 		$val->add_callable('exvalidation');
-		$val->add_field('code','学科コード','required|max_length[2]');
+		$val->add_field('code','学科コード','required|max_length[2]')->add_rule('unique', 'course', 'code');
 		$val->add_field('name','学科名','trim|required|max_length[64]');
 		$val->add_field('year_system','年制','trim|required|max_length[10]');
 		$val->add_field('college_id','カレッジID','required|max_length[10]');
