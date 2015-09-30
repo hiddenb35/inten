cda09ed064fc9c38429c5c52c260f0aa54b20036
k013c1118 <k013c1118@it-neec.jp>
2015-09-30 17:03:31 +0900

    issue#151  生徒の一覧画面にデータを送信

--- fuel/app/classes/controller/student.php
+++ fuel/app/classes/controller/student.php
@@ -18,6 +18,34 @@ class Controller_Student extends Controller_Loggedin
 	{
 		$this->template->title = '生徒の一覧';
 		$this->template->content = View::forge('student/student_list');
+		$this->template->content->set('student_lists',$this->_get_list());
 	}
 
+	private function _get_list()
+	{
+
+		$student_lists = array();
+
+		foreach(Model_Student::find('all') as $student)
+		{
+			$array = array();
+			$array['id'] = $student['id'];
+			$array['number'] = $student['username'];
+			$array['full_name'] = $student['last_name'] . ' ' . $student['first_name'];
+			$array['full_name_kana'] = $student['last_name_kana'] . ' ' . $student['first_name_kana'];
+			$array['birthday'] = $student['birthday'];
+			$array['email'] = $student['email'];
+			$array['gender'] = $student['gender'];
+			$array['last_login'] = $student['last_login'];
+			$array['created_at'] = $student['created_at'];
+			$array['updated_at'] = $student['updated_at'];
+			$array['class_name'] = $student->class->name;
+			$array['major_name'] = (isset($student->major->name)) ? $student->major->name : '';
+
+
+			$student_lists[] = $array;
+		}
+
+		return $student_lists;
+	}
 }
