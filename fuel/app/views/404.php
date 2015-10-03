3f679520f7cd9db975b28c5e22ee4f57633d3373
Shota Narumi <k013c1112@it-neec.jp>
2015-10-04 02:57:58 +0900

    issue#197　404ページの修正
    ・不要行の削除

--- fuel/app/views/404.php
+++ fuel/app/views/404.php
@@ -1,12 +1,5 @@
 <div id="404">
 	<div id="404page" class="container-fluid" style="font-family: 游ゴシック体, 'Yu Gothic', YuGothic, 'ヒラギノ角ゴシック Pro', 'Hiragino Kaku Gothic Pro', メイリオ, Meiryo, Osaka, 'ＭＳ Ｐゴシック', 'MS PGothic', sans-serif; background-color: #cccccc;">
-		<div class="row">
-			<div class="col-md-3"></div>
-				<div class="col-md-6">
-					<div></div>
-					<h1 class="text-center" style="font-size: 4em;">存在しないページです。</h1>
-				</div>
-			<div class="col-md-3"></div>
-		</div>
+		<h1 class="text-center" style="font-size: 4em;">存在しないページです。</h1>
 	</div>
 </div>
