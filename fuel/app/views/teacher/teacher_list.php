e9eba9a2f5e90366521ca310621a25ee31747ab1
Shota Narumi <k013c1112@it-neec.jp>
2015-10-04 01:19:58 +0900

    issue#159 教員一覧画面の修正
    ・テーブルデザインの変更

--- fuel/app/views/teacher/teacher_list.php
+++ fuel/app/views/teacher/teacher_list.php
@@ -9,25 +9,26 @@
 <!-- Main content -->
 <section class="content">
 <div id="TEACHER_LIST">
-    <div class="table-responsive">
-        <table class="table table-borderd table-striped table-hover">
-            <thead>
-                <tr>
-                    <th>教員番号</th>                   <!-- 芦沢ゾーンここから -->
-                    <th>姓</th>
-                    <th>名</th>
-                    <th>セイ</th>
-                    <th>メイ</th>
-                    <th>生年月日(西暦)</th>
-                    <th>生年月日(月)</th>
-                    <th>生年月日(日)</th>
-                    <th>E-mail</th>
-                    <th>性別</th>
-                </tr>
+    <div class="container-fluid">
+        <div class="table-responsive">
+            <table class="table table-bordered table-striped table-hover table-condensed">
+                <thead>
+                    <tr class="info">
+                        <th>教員番号</th>                   <!-- 芦沢ゾーンここから -->
+                        <th>姓</th>
+                        <th>名</th>
+                        <th>セイ</th>
+                        <th>メイ</th>
+                        <th>生年月日(西暦)</th>
+                        <th>生年月日(月)</th>
+                        <th>生年月日(日)</th>
+                        <th>E-mail</th>
+                        <th>性別</th>
+                    </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>######</td>
+                        <td>#######</td>
                         <td>加藤</td>
                         <td>拓磨</td>
                         <td>カトウ</td>
@@ -38,8 +39,33 @@
                         <td>k013c1140@it-neec.jp</td>
                         <td>女</td>
                     </tr>
+                    <tr>
+                        <td>#######</td>
+                        <td>田中</td>
+                        <td>太郎</td>
+                        <td>タナカ</td>
+                        <td>タロウ</td>
+                        <td>1994</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>k013c1141@it-neec.jp</td>
+                        <td>男</td>
+                    </tr>
+                    <tr>
+                        <td>#######</td>
+                        <td>蒲田</td>
+                        <td>太郎</td>
+                        <td>カマタ</td>
+                        <td>タロウ</td>
+                        <td>1994</td>
+                        <td>11</td>
+                        <td>11</td>
+                        <td>k013c1142@it-neec.jp</td>
+                        <td>男</td>
+                    </tr>
                 </tbody>
             </table>
         </div>
     </div>
+</div>
 </section>
