a6728a780d9e69ba6b69c614e27970313c50572d
Shota Narumi <k013c1112@it-neec.jp>
2015-10-04 02:48:10 +0900

    issue#197 404画面の修正
    ・表示されるテキストの変更
    ・レスポンシブ適用

--- fuel/app/views/404.php
+++ fuel/app/views/404.php
@@ -1,8 +1,12 @@
 <div id="404">
-	<div id="404page" style="font-family: fantasy; background-color: #cccccc;">
-		<div class="text-center">
-			<h1 style="font-size: 12em;">404</h1>
-			<h1 style="font-size: 9em;">NOT FOUND</h1>
+	<div id="404page" class="container-fluid" style="font-family: 游ゴシック体, 'Yu Gothic', YuGothic, 'ヒラギノ角ゴシック Pro', 'Hiragino Kaku Gothic Pro', メイリオ, Meiryo, Osaka, 'ＭＳ Ｐゴシック', 'MS PGothic', sans-serif; background-color: #cccccc;">
+		<div class="row">
+			<div class="col-md-3"></div>
+				<div class="col-md-6">
+					<div></div>
+					<h1 class="text-center" style="font-size: 4em;">存在しないページです。</h1>
+				</div>
+			<div class="col-md-3"></div>
 		</div>
 	</div>
 </div>
