7d8a2d353cdfaf652551e3a72a8812d339faafbc
Keita Kasai <k013c1345@it-neec.jp>
2015-10-07 05:00:24 +0000

    issue#150 生徒一覧画面のデータを動的に表示するようにしました。

--- fuel/app/views/student/student_list.php
+++ fuel/app/views/student/student_list.php
@@ -1,63 +1,45 @@
-<section class="content-header">
-    <h1>
-        生徒一覧
-    </h1>
-    <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-    </ol>
-</section>
-<!-- Main content -->
-<section class="content">
 <div id="STUDENT_LIST">
-    <div class="container-fluid">
-        <div class="table-responsive">
-            <table class="table table-bordered table-striped table-hover table-condensed">
-                <thead>
-                    <tr class="info">
-                        <th>学籍番号</th>                   <!-- 芦沢ゾーンここから -->
-                        <th>フルネーム</th>
-                        <th>フルネーム(カナ)</th>
-                        <th>生年月日</th>
-                        <th>E-Mail</th>
-                        <th>性別</th>
-                        <th>クラス名</th>
-                        <th>専攻名</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>k013c1140</td>
-                        <td>加藤 拓磨</td>
-                        <td>カトウ タクマ</td>
-                        <td>1994/10/12</td>
-                        <td>k013c1140@it-neec.jp</td>
-                        <td>女</td>
-                        <td>IS07-1</td>
-                        <td>システム専攻</td>                      <!-- 芦沢ゾーンここまで -->
-                    </tr>
-                    <tr>
-                        <td>k013c1141</td>
-                        <td>田中 太郎</td>
-                        <td>タナカ タロウ</td>
-                        <td>1994/1/2</td>
-                        <td>k013c1141@it-neec.jp</td>
-                        <td>女</td>
-                        <td>IS07-1</td>
-                        <td>システム専攻</td>                      <!-- 芦沢ゾーンここまで -->
-                    </tr>
-                    <tr>
-                        <td>k013c1142</td>
-                        <td>蒲田 太郎</td>
-                        <td>カマタ タロウ</td>
-                        <td>1994/3/4</td>
-                        <td>k013c1142@it-neec.jp</td>
-                        <td>女</td>
-                        <td>IS07-1</td>
-                        <td>ウェブ専攻</td>                      <!-- 芦沢ゾーンここまで -->
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
+	<section class="content-header">
+		<h1>
+			生徒一覧
+		</h1>
+		<ol class="breadcrumb">
+			<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+		</ol>
+	</section>
+	<!-- Main content -->
+	<section class="content">
+		<div class="container-fluid">
+			<div class="table-responsive">
+				<table class="table table-bordered table-striped table-hover table-condensed">
+					<thead>
+					<tr class="info">
+						<th>学籍番号</th>
+						<th>フルネーム</th>
+						<th>フルネーム(カナ)</th>
+						<th>生年月日</th>
+						<th>E-Mail</th>
+						<th>性別</th>
+						<th>クラス名</th>
+						<th>専攻名</th>
+					</tr>
+					</thead>
+					<tbody>
+					<?php foreach($student_lists as $student_list): ?>
+					<tr>
+						<td><?php echo $student_list['number']; ?></td>
+						<td><?php echo $student_list['full_name']; ?></td>
+						<td><?php echo $student_list['full_name_kana']; ?></td>
+						<td><?php echo $student_list['birthday']; ?></td>
+						<td><?php echo $student_list['email']; ?></td>
+						<td><?php echo $student_list['gender']; ?></td>
+						<td><?php echo $student_list['class_name']; ?></td>
+						<td><?php echo $student_list['major_name']; ?></td>
+					</tr>
+					<?php endforeach; ?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</section>
 </div>
-</section>
