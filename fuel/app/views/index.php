429807e67523042ca51552af14ae00bb597b6849
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-10-26 00:23:28 +0900

    issue#225 時間割表示画面で時間割情報がない場合は「時間割が登録されていません」と表示するように修正

--- fuel/app/views/index.php
+++ fuel/app/views/index.php
@@ -10,22 +10,26 @@
 	</section>
 	<!-- Main content -->
 	<section class="content">
-		<div class="box box-info">
-			<div class="box-header with-border">
-				<h3 class="box-title"><?php echo $name ?></h3>
-				<div id="week-select" class="visible-xs">
-					<button type="button" class="btn btn-default">月</button>
-					<button type="button" class="btn btn-default">火</button>
-					<button type="button" class="btn btn-default">水</button>
-					<button type="button" class="btn btn-default">木</button>
-					<button type="button" class="btn btn-default">金</button>
+		<?php if(empty($html)): ?>
+			<h4>時間割が登録されていません</h4>
+		<?php else: ?>
+			<div class="box box-info">
+				<div class="box-header with-border">
+					<h3 class="box-title"><?php echo $name ?></h3>
+					<div id="week-select" class="visible-xs">
+						<button type="button" class="btn btn-default">月</button>
+						<button type="button" class="btn btn-default">火</button>
+						<button type="button" class="btn btn-default">水</button>
+						<button type="button" class="btn btn-default">木</button>
+						<button type="button" class="btn btn-default">金</button>
+					</div>
+				</div>
+				<div class="box-body">
+					<div class="container-fluid">
+						<?php echo Timetable::generate($html); ?>
+					</div>
 				</div>
 			</div>
-			<div class="box-body">
-				<div class="container-fluid">
-					<?php echo Timetable::generate($html); ?>
-				</div>
-			</div>
-		</div>
+		<?php endif; ?>
 	</section>
 </div>
