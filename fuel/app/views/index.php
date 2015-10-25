9bc949bad160a45bf63c56f79b92f497ad082ce7
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-10-26 00:13:26 +0900

    issue#225 時間割表示画面に生徒自身が該当する時間割情報を表示するように変更

--- fuel/app/views/index.php
+++ fuel/app/views/index.php
@@ -12,7 +12,7 @@
 	<section class="content">
 		<div class="box box-info">
 			<div class="box-header with-border">
-				<h3 class="box-title">ここに時間割のタイトルを表示</h3>
+				<h3 class="box-title"><?php echo $name ?></h3>
 				<div id="week-select" class="visible-xs">
 					<button type="button" class="btn btn-default">月</button>
 					<button type="button" class="btn btn-default">火</button>
@@ -23,84 +23,7 @@
 			</div>
 			<div class="box-body">
 				<div class="container-fluid">
-					<table class="table table-bordered table-responsive table-striped">
-						<thead>
-							<tr class="bg-primary">
-								<th class="text-center">#</th>
-								<th class="text-center">月曜日</th>
-								<th class="text-center">火曜日</th>
-								<th class="text-center">水曜日</th>
-								<th class="text-center">木曜日</th>
-								<th class="text-center">金曜日</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<th class="text-center bg-info"><span>1時限目</span><br>9:00-9:50</th>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-							</tr>
-							<tr>
-								<th class="text-center bg-info"><span>2時限目</span><br>10:00-10:50</th>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-							</tr>
-							<tr>
-								<th class="text-center bg-info"><span>3時限目</span><br>11:00-11:50</th>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-							</tr>
-							<tr>
-								<th class="text-center bg-info"><span>4時限目</span><br>12:00-12:50</th>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-							</tr>
-							<tr>
-								<th class="text-center bg-info"><span>5時限目</span><br>13:00-13:50</th>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-							</tr>
-							<tr>
-								<th class="text-center bg-info"><span>6時限目</span><br>14:00-14:50</th>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-							</tr>
-							<tr>
-								<th class="text-center bg-info"><span>7時限目</span><br>15:00-15:50</th>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-							</tr>
-							<tr>
-								<th class="text-center bg-info"><span>8時限目</span><br>16:00-16:50</th>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-								<td class="text-center"><p class="subject"></p><p class="teacher"></p><p class="classroom"></p><p class="note"></p><span class="badge bg-green">備考あり</span></td>
-							</tr>
-						</tbody>
-					</table>
+					<?php echo Timetable::generate($html); ?>
 				</div>
 			</div>
 		</div>
