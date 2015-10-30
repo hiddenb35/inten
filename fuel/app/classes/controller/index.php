b7384fef19fe410f46d09ccd3a33cbaa814e7dda
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-10-30 17:54:31 +0900

    issue#231 教員のトップページの修正
    教員アカウントのトップページを自分が担当するクラスの中の１つの時間割を表示するように変更

--- fuel/app/classes/controller/index.php
+++ fuel/app/classes/controller/index.php
@@ -24,7 +24,23 @@ class Controller_Index extends Controller_Loggedin
 		}
 		else
 		{
-			Response::redirect('class/myclass');
+			$teacher = Model_Teacher::find($this->get_id());
+			$timetable = null;
+			if(!empty($teacher->classes))
+			{
+				$timetable = Model_Timetable::find('first', array(
+					'where' => array(
+						array('class_id', '=', array_shift($teacher->classes)->id),
+						array('is_active', '=', 1),
+					),
+					'order_by' => array('updated_at' => 'desc', 'created_at' => 'desc'),
+				));
+			}
+
+			$name = (is_null($timetable)) ? '' : $timetable->name;
+			$html = (is_null($timetable)) ? array() : json_decode($timetable->html, true);
+			$this->template->content->set('name', $name);
+			$this->template->content->set('html', $html);
 		}
 	}
 }
