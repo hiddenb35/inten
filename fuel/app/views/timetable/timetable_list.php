70fbdfe822f59839f4956723309f86934d820c5d
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-11-06 01:14:37 +0900

    issue#233 時間割一覧ページの静的コーディング仮

---
+++ fuel/app/views/timetable/timetable_list.php
@@ -0,0 +1,40 @@
+<div id="COLLEGE_LIST">
+	<section class="content-header">
+		<h1>
+			IS07-1 時間割一覧
+		</h1>
+		<ol class="breadcrumb">
+			<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+		</ol>
+	</section>
+	<!-- Main content -->
+	<section class="content">
+		<div class="box">
+			<div class="box-body">
+				<table class="table table-bordered table-striped" id="college_list_table">
+					<tbody>
+					<tr>
+						<th>時間割名</th>
+						<th>作成日</th>
+						<th>更新日</th>
+						<th>ステータス</th>
+						<th></th>
+					</tr>
+					<tr>
+						<td>hoeg</td>
+						<td>hoge</td>
+						<td>hoge</td>
+						<td>hoge</td>
+						<td>
+							<a href="#" class="btn btn-primary btn-sm">編集</a>
+							<a href="#" class="btn btn-danger btn-sm">削除</a>
+						</td>
+					</tr>
+					</tbody>
+				</table>
+			</div><!-- /.box-body -->
+		</div>
+		<div id="edit_modal_content" class="alert">
+		</div>
+	</section>
+</div>
