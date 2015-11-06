d12694294eeba93560ccb48a5713aa9c01ca7ade
Shota Narumi <k013c1112@it-neec.jp>
2015-11-06 13:40:17 +0900

    issue#234 時間割表示画面のコード修正
    ・セルの高さ揃え
    ・フォントサイズ変更
    ・スマホ時の曜日選択ボタンのデザイン変更

--- fuel/app/views/index.php
+++ fuel/app/views/index.php
@@ -16,15 +16,15 @@
 			<div class="box box-info">
 				<div class="box-header with-border">
 					<h3 class="box-title"><?php echo $name ?></h3>
-					<div id="week-select" class="visible-xs">
-						<button type="button" class="btn btn-default">月</button>
-						<button type="button" class="btn btn-default">火</button>
-						<button type="button" class="btn btn-default">水</button>
-						<button type="button" class="btn btn-default">木</button>
-						<button type="button" class="btn btn-default">金</button>
-					</div>
 				</div>
 				<div class="box-body">
+					<div id="week-select" class="visible-xs row">
+						<button type="button" class="btn btn-default col-xs-2 col-xs-offset-1">月</button>
+						<button type="button" class="btn btn-default col-xs-2">火</button>
+						<button type="button" class="btn btn-default col-xs-2">水</button>
+						<button type="button" class="btn btn-default col-xs-2">木</button>
+						<button type="button" class="btn btn-default col-xs-2">金</button>
+					</div>
 					<div class="container-fluid">
 						<?php echo Timetable::generate($html); ?>
 					</div>
