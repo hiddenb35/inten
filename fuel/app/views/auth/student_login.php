9ab99d196acdfef9f9133a2722a62588df8a4524
Shota Narumi <k013c1112@it-neec.jp>
2015-11-06 16:43:00 +0900

    issue#236 生徒用ログイン画面の修正
    ・画面を暗くしました
    ・教員用ログイン画面へのリンクの作成

--- fuel/app/views/auth/student_login.php
+++ fuel/app/views/auth/student_login.php
@@ -1,45 +1,51 @@
 <div id="LOGIN">
-<div class="container">
-    <div class="row">
-        <div class="col-xs-12">
-            <div class="row">
-                <!--            左               -->
-                <div class="col-md-4"></div>
-                <!--            左               -->
+	<div id="login_body">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="row">
+						<!--            左               -->
+						<div class="col-md-4"></div>
+						<!--            左               -->
 
-                <!--            コンテンツ              -->
-                <div class="col-md-4">
-                    <div class="login-box">
-                        <div class="login-box-header text-center h1">
-                            <b class="hidden-xs" >Student_Login</b>
-                            <b class="visible-xs">Login</b>
-                        </div>
-                        <div class="login-box-body">
-                            <form  class="form-horizontal text-center" id="login_form" method="post">
-                                <div class="form-group input-group">
-                                    <span class="input-group-addon"><i class="ion-person"></i></span>
-                                    <input type="text" name="username" class="form-control input-lg" placeholder="Username">
-                                </div>
-                                <div class="form-group input-group">
-                                    <span class="input-group-addon"><i class="ion-locked"></i></span>
-                                    <input type="password" name="password" class="form-control input-lg" placeholder="Password">
-                                </div>
-                                <div class="form-group text-center">
-                                    <button type="submit" class="btn btn-block btn-primary btn-lg login-button">Login
-                                        <!-- <span class="glyphicon glyphicon-log-in"></span> -->
-                                        <span class="ion-power"></span>
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                <!--            コンテンツ            -->
-                <!--            右               -->
-                <div class="col-md-4"></div>
-                <!--            右               -->
-            </div>
-        </div>
-    </div>
-</div>
+						<!--            コンテンツ              -->
+						<div class="col-md-4">
+							<div class="login-box">
+								<div class="login-box-header text-center h1">
+									<b class="hidden-xs">Student_Login</b>
+									<b class="visible-xs">Login</b>
+								</div>
+								<div class="login-box-body">
+									<form class="form-horizontal text-center" id="login_form" method="post">
+										<div class="form-group input-group">
+											<span class="input-group-addon"><i class="ion-person"></i></span>
+											<input type="text" name="username" class="form-control input-lg"
+												   placeholder="Username">
+										</div>
+										<div class="form-group input-group">
+											<span class="input-group-addon"><i class="ion-locked"></i></span>
+											<input type="password" name="password" class="form-control input-lg"
+												   placeholder="Password">
+										</div>
+										<div class="form-group text-center">
+											<button type="submit" class="btn btn-block btn-primary btn-lg login-button">
+												Login
+												<!-- <span class="glyphicon glyphicon-log-in"></span> -->
+												<span class="ion-power"></span>
+											</button>
+										</div>
+									</form>
+								</div>
+								<div class="text-right" id="change_login"><a href="/auth/tlogin">教員の方はこちら</a></div>
+							</div>
+						</div>
+						<!--            コンテンツ            -->
+						<!--            右               -->
+						<div class="col-md-4"></div>
+						<!--            右               -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
