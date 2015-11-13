bec745964e8cf93ad28d2f9eccbe5d0ea819abb4
Keita Kasai <k013c1345@it-neec.jp>
2015-11-13 01:20:57 +0000

    issue#246 管理者用のサイドバーを作成しました。

---
+++ fuel/app/views/template/after_login/sidebar_admin.php
@@ -0,0 +1,80 @@
+<!-- sidebar: style can be found in sidebar.less -->
+<section class="sidebar">
+	<!-- sidebar menu: : style can be found in sidebar.less -->
+	<ul class="sidebar-menu">
+		<li class="header">MAIN NAVIGATION</li>
+		<li class="treeview">
+			<a href="/">
+				<i class="fa ion-clock"></i> <span>時間割表示</span>
+			</a>
+		</li>
+		<li class="treeview">
+			<a href="/timetable/list">
+				<i class="fa ion-university"></i> <span>時間割</span></i>
+			</a>
+		</li>
+		<li class="treeview">
+			<a href="/admin/college">
+				<i class="fa ion-university"></i> <span>カレッジ情報</span>
+			</a>
+
+		</li>
+		<li class="treeview">
+			<a href="/admin/course">
+				<i class="fa ion-university"></i> <span>学科情報</span>
+			</a>
+		</li>
+		<li class="treeview">
+			<a href="/admin/class">
+				<i class="fa ion-university"></i> <span>クラス情報</span>
+			</a>
+		</li>
+		<li class="treeview">
+			<a href="/admin/major">
+				<i class="fa ion-university"></i> <span>専攻情報</span>
+			</a>
+		</li>
+		<li class="treeview">
+			<a href="/">
+				<i class="fa ion-university"></i> <span>教員情報</span> <i class="fa fa-angle-down pull-right"></i>
+			</a>
+			<ul class="treeview-menu">
+				<li><a href="/teacher/list"><i class="fa fa-circle-o"></i>教員一覧</a></li>
+				<li><a href="/teacher/edit"><i class="fa fa-circle-o"></i>教員編集</a></li>
+				<li><a href="/teacher/add"><i class="fa fa-circle-o"></i>教員追加</a></li>
+				<li><a href="/teacher/attachment_lesson"><i class="fa fa-circle-o"></i>授業割り当て</a></li>
+				<li><a href="/class/myclass"><i class="fa fa-circle-o"></i>担当するクラス一覧</a></li>
+			</ul>
+		</li>
+		<li class="treeview">
+			<a href="/">
+				<i class="fa ion-university"></i> <span>生徒情報</span> <i class="fa fa-angle-down pull-right"></i>
+			</a>
+			<ul class="treeview-menu">
+				<li><a href="/student/list"><i class="fa fa-circle-o"></i>生徒一覧</a></li>
+				<li><a href="/student/edit"><i class="fa fa-circle-o"></i>生徒編集</a></li>
+				<li><a href="/student/add"><i class="fa fa-circle-o"></i>生徒追加</a></li>
+			</ul>
+		</li>
+		<li class="treeview">
+			<a href="#">
+				<i class="fa ion-university"></i> <span>授業情報</span> <i class="fa fa-angle-down pull-right"></i>
+			</a>
+			<ul class="treeview-menu">
+				<li><a href="/lesson/list"><i class="fa fa-circle-o"></i>授業一覧</a></li>
+				<li><a href="/lesson/add"><i class="fa ion-university"></i> <span>授業追加</span></a></li>
+			</ul>
+		</li>
+		<li>
+			<a href="/attendance/lesson_list">
+				<i class="fa ion-university"></i> <span>出席を取る</span>
+			</a>
+		</li>
+		<li>
+			<a href="/setting/pass_update">
+				<i class="fa ion-settings"></i> <span>パスワード変更</span>
+			</a>
+		</li>
+	</ul>
+</section>
+<!-- /.sidebar -->
