0d122a394b9d10f3fb1e1e14aacc68257091ce15
kasai keita <kasai@example.local>
2015-11-20 15:23:27 +0900

    issue#264 自分の担当する授業の出席率一覧画面動的表示するようにしました。

--- fuel/app/views/attendance/attendance_rate_list.php
+++ fuel/app/views/attendance/attendance_rate_list.php
@@ -22,34 +22,15 @@
 						</tr>
 						</thead>
 						<tbody class="row">
-						<tr>
-							<td>K013C1296</td>
-							<td>加藤拓磨</td>
-							<td class="hidden-xs">カトウタクマ</td>
-							<td class="col-md-2 hidden-xs progress progress-xs progress-striped active"><div class="progress-bar progress-bar-primary" style="width: 100%;"></div></td>
-							<td class="col-md-1 badge bg-aqua"><span class="badge bg-aqua">100%</span></td>
-						</tr>
-						<tr>
-							<td>K013C1296</td>
-							<td>加藤拓磨</td>
-							<td class="hidden-xs">カトウタクマ</td>
-							<td class="col-md-2 hidden-xs progress progress-xs progress-striped active"><div class="progress-bar progress-bar-primary" style="width: 75%;"></div></td>
-							<td class="col-md-1 badge bg-aqua">75%</td>
-						</tr>
-						<tr>
-							<td>K013C1296</td>
-							<td>加藤拓磨</td>
-							<td class="hidden-xs">カトウタクマ</td>
-							<td class="col-md-2 hidden-xs progress progress-xs progress-striped active"><div class="progress-bar progress-bar-danger" style="width: 74%;"></div></td>
-							<td class="col-md-1 badge bg-red">74%</td>
-						</tr>
-						<tr>
-							<td>K013C1296</td>
-							<td>加藤拓磨</td>
-							<td class="hidden-xs">カトウタクマ</td>
-							<td class="col-md-2 hidden-xs progress progress-xs progress-striped active"><div class="progress-bar progress-bar-danger" style="width: 5%;"></div></td>
-							<td class="col-ms-1 badge bg-red">5%</td>
-						</tr>
+						<?php foreach($student_lists as $student_list): ?>
+							<tr>
+								<td><?php echo $student_list['id']; ?></td>
+								<td><?php echo $student_list['full_name']; ?></td>
+								<td class="hidden-xs"><?php echo $student_list['full_name_kana']; ?></td>
+								<td class="col-md-2 hidden-xs progress progress-xs progress-striped active"><div class="progress-bar <?php echo $student_list['rate_bar_class']; ?>" style="width: <?php echo $student_list['rate']; ?>;"></div></td>
+								<td class="col-md-1 badge <?php echo $student_list['rate_bg_class']; ?>"><?php echo $student_list['rate']; ?></td>
+							</tr>
+						<?php endforeach; ?>
 						</tbody>
 					</table>
 				</div>
