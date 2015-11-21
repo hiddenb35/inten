5112252e1e030c5d373e8a24b6b32343ee3d599c
Shota Narumi <k013c1112@it-neec.jp>
2015-11-21 15:23:20 +0900

    issue#270 担当する授業の出席率一覧画面の作成

---
+++ fuel/app/views/attendance/total_attendance_rate_list.php
@@ -0,0 +1,59 @@
+<div id="TOTAL_ATTENDANCE_RATE_LIST">
+	<section class="content-header">
+		<h1>
+			担当する授業の出席率一覧画面
+		</h1>
+		<ol class="breadcrumb">
+			<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+		</ol>
+	</section>
+	<!-- Main content -->
+	<section class="content">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="table-responsive">
+					<table class="table table-bordered table-striped table-hover table-condensed">
+						<thead>
+						<tr>
+							<th>学籍番号</th>
+							<th>名前</th>
+							<th class="hidden-xs">フリガナ</th>
+							<th colspan="2">出席率</th>
+						</tr>
+						</thead>
+						<tbody class="row">
+						<tr>
+							<td>K013C1296</td>
+							<td>加藤拓磨</td>
+							<td class="hidden-xs">カトウタクマ</td>
+							<td class="col-md-2 hidden-xs progress progress-xs progress-striped active"><div class="progress-bar progress-bar-primary" style="width: 100%;"></div></td>
+							<td class="col-md-1 badge bg-aqua"><span class="badge bg-aqua">100%</span></td>
+						</tr>
+						<tr>
+							<td>K013C1296</td>
+							<td>加藤拓磨</td>
+							<td class="hidden-xs">カトウタクマ</td>
+							<td class="col-md-2 hidden-xs progress progress-xs progress-striped active"><div class="progress-bar progress-bar-primary" style="width: 75%;"></div></td>
+							<td class="col-md-1 badge bg-aqua">75%</td>
+						</tr>
+						<tr>
+							<td>K013C1296</td>
+							<td>加藤拓磨</td>
+							<td class="hidden-xs">カトウタクマ</td>
+							<td class="col-md-2 hidden-xs progress progress-xs progress-striped active"><div class="progress-bar progress-bar-danger" style="width: 74%;"></div></td>
+							<td class="col-md-1 badge bg-red">74%</td>
+						</tr>
+						<tr>
+							<td>K013C1296</td>
+							<td>加藤拓磨</td>
+							<td class="hidden-xs">カトウタクマ</td>
+							<td class="col-md-2 hidden-xs progress progress-xs progress-striped active"><div class="progress-bar progress-bar-danger" style="width: 5%;"></div></td>
+							<td class="col-ms-1 badge bg-red">5%</td>
+						</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
