58c13ff6f6d7a2d2fc659eca69401a63d000c8f6
Shota Narumi <k013c1112@it-neec.jp>
2015-11-26 02:23:22 +0900

    issue#278 出席を取る画面の作成
    ・PC用出席を取る画面の作成

---
+++ fuel/app/views/attendance/take_attendance.php
@@ -0,0 +1,126 @@
+<div id="TAKE_ATTENDANCE">
+	<section class="content-header">
+		<h1>
+			担当する授業の出席を取る画面
+		</h1>
+		<ol class="breadcrumb">
+			<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+		</ol>
+	</section>
+	<!-- Main content -->
+	<section class="content">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm-6 col-md-4 col-lg-3">
+					<div class="box">
+						<div class="box-header">
+							<h3 class="box-title">K013C1296</h3>
+							<div>ササキ ユリ</div>
+							<div class="box-tools pull-right">
+								<button type="button" class="btn btn-box-tool" data-widget="collapse"><i
+										class="fa fa-minus"></i>
+								</button>
+							</div>
+						</div>
+						<div class="box-header">
+							<div class="h3 text-right">佐々木 佑梨</div>
+						</div>
+						<div class="box-footer text-center">
+							<button type="button" class="btn btn-primary">出席</button>
+							<button type="button" class="btn btn-warning">遅刻</button>
+							<button type="button" class="btn btn-danger">欠席</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4 col-lg-3">
+					<div class="box">
+						<div class="box-header">
+							<h3 class="box-title">K013C1296</h3>
+							<div>ササキ ユリ</div>
+							<div class="box-tools pull-right">
+								<button type="button" class="btn btn-box-tool" data-widget="collapse"><i
+										class="fa fa-minus"></i>
+								</button>
+							</div>
+						</div>
+						<div class="box-header">
+							<div class="h3 text-right">佐々木 佑梨</div>
+						</div>
+						<div class="box-footer text-center">
+							<button type="button" class="btn btn-primary">出席</button>
+							<button type="button" class="btn btn-warning">遅刻</button>
+							<button type="button" class="btn btn-danger">欠席</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4 col-lg-3">
+					<div class="box">
+						<div class="box-header">
+							<h3 class="box-title">K013C1296</h3>
+							<div>ササキ ユリ</div>
+							<div class="box-tools pull-right">
+								<button type="button" class="btn btn-box-tool" data-widget="collapse"><i
+										class="fa fa-minus"></i>
+								</button>
+							</div>
+						</div>
+						<div class="box-header">
+							<div class="h3 text-right">佐々木 佑梨</div>
+						</div>
+						<div class="box-footer text-center">
+							<button type="button" class="btn btn-primary">出席</button>
+							<button type="button" class="btn btn-warning">遅刻</button>
+							<button type="button" class="btn btn-danger">欠席</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4 col-lg-3">
+					<div class="box">
+						<div class="box-header">
+							<h3 class="box-title">K013C1296</h3>
+							<div>ササキ ユリ</div>
+							<div class="box-tools pull-right">
+								<button type="button" class="btn btn-box-tool" data-widget="collapse"><i
+										class="fa fa-minus"></i>
+								</button>
+							</div>
+						</div>
+						<div class="box-header">
+							<div class="h3 text-right">佐々木 佑梨</div>
+						</div>
+						<div class="box-footer text-center">
+							<button type="button" class="btn btn-primary">出席</button>
+							<button type="button" class="btn btn-warning">遅刻</button>
+							<button type="button" class="btn btn-danger">欠席</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4 col-lg-3">
+					<div class="box">
+						<div class="box-header">
+							<h3 class="box-title">K013C1296</h3>
+							<div>ササキ ユリ</div>
+							<div class="box-tools pull-right">
+								<button type="button" class="btn btn-box-tool" data-widget="collapse"><i
+										class="fa fa-minus"></i>
+								</button>
+							</div>
+						</div>
+						<div class="box-header">
+							<div class="h3 text-right">佐々木 佑梨</div>
+						</div>
+						<div class="box-footer text-center">
+							<button type="button" class="btn btn-primary">出席</button>
+							<button type="button" class="btn btn-warning">遅刻</button>
+							<button type="button" class="btn btn-danger">欠席</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="text-center">
+				<button type="submit" class="btn btn-primary">送信</button>
+				<button type="reset" class="btn btn-danger">リセット</button>
+			</div>
+		</div>
+	</section>
+</div>
