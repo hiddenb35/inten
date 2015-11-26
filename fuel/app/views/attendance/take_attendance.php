86a2dfbc02b0af477f73992c7be4c04cc560065b
Shota Narumi <k013c1112@it-neec.jp>
2015-11-26 23:02:02 +0900

    issue#278 出席を取る画面の作成
    ・スマホ用画面の作成

--- fuel/app/views/attendance/take_attendance.php
+++ fuel/app/views/attendance/take_attendance.php
@@ -10,7 +10,7 @@
 	<!-- Main content -->
 	<section class="content">
 		<div class="container-fluid">
-			<div class="row">
+			<div class="row pc-size">
 				<div class="col-sm-6 col-md-4 col-lg-3">
 					<div class="box">
 						<div class="box-header">
@@ -18,7 +18,7 @@
 							<div>ササキ ユリ</div>
 							<div class="box-tools pull-right">
 								<button type="button" class="btn btn-box-tool" data-widget="collapse"><i
-										class="fa fa-minus"></i>
+											class="fa fa-minus"></i>
 								</button>
 							</div>
 						</div>
@@ -39,7 +39,7 @@
 							<div>ササキ ユリ</div>
 							<div class="box-tools pull-right">
 								<button type="button" class="btn btn-box-tool" data-widget="collapse"><i
-										class="fa fa-minus"></i>
+											class="fa fa-minus"></i>
 								</button>
 							</div>
 						</div>
@@ -60,7 +60,7 @@
 							<div>ササキ ユリ</div>
 							<div class="box-tools pull-right">
 								<button type="button" class="btn btn-box-tool" data-widget="collapse"><i
-										class="fa fa-minus"></i>
+											class="fa fa-minus"></i>
 								</button>
 							</div>
 						</div>
@@ -81,7 +81,7 @@
 							<div>ササキ ユリ</div>
 							<div class="box-tools pull-right">
 								<button type="button" class="btn btn-box-tool" data-widget="collapse"><i
-										class="fa fa-minus"></i>
+											class="fa fa-minus"></i>
 								</button>
 							</div>
 						</div>
@@ -102,7 +102,7 @@
 							<div>ササキ ユリ</div>
 							<div class="box-tools pull-right">
 								<button type="button" class="btn btn-box-tool" data-widget="collapse"><i
-										class="fa fa-minus"></i>
+											class="fa fa-minus"></i>
 								</button>
 							</div>
 						</div>
@@ -113,6 +113,105 @@
 							<button type="button" class="btn btn-primary">出席</button>
 							<button type="button" class="btn btn-warning">遅刻</button>
 							<button type="button" class="btn btn-danger">欠席</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!------------------------スマホ・タブレットサイズ-------------------------------->
+			<div class="sp-size">
+				<div class="text-center row">
+					<button type="button" class="btn col-xs-offset-1 col-xs-10" data-toggle="modal" data-target="#myModal
+					">出席を取る</button>
+				</div>
+				<!-------------------------modalここから---------------------------->
+				<div class="modal fade in" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<div class="modal-inner">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">閉じる</span><span class="sr-only">Close</span></button>
+									<h3 class="modal-title" id="myModalLabel">K013C1296</h3>
+									<h3>ササキユリ</h3>
+									<h2 class="text-center">佐々木 佑梨</h2>
+								</div>
+								<div class="modal-body">
+									<input type="image" src="../img/attendance.png">
+									<input type="image" src="../img/absence.png">
+								</div>
+								<div class="modal-footer">
+									<div class="text-center">
+										<button type="button" class="btn btn-lg">戻る</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!----------------------ここまで---------------------------->
+				<div class="panel-group" id="accordion">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<div class="panel-title h4">
+								<!--------------学生の数だけstudentの数値が増加する---------------->
+								<div class="text-center" data-toggle="collapse" data-parent="#accordion" href="#student1">
+									<div class="cell create-circle"></div>
+									<div class="cell">K013C1296 佐々木 佑梨</div>
+									<ul class="cell">
+										<li>K013C1296</li>
+										<li>佐々木 佑梨</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div id="student1" class="panel-collapse collapse">
+							<div class="panel-body text-center">
+								<button type="button" class="btn btn-primary">出席</button>
+								<button type="button" class="btn btn-warning">遅刻</button>
+								<button type="button" class="btn btn-danger">欠席</button>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<div class="panel-title h4">
+								<div data-toggle="collapse" data-parent="#accordion" href="#student2">
+									<div class="cell create-circle"></div>
+									<div class="cell">K013C1296 佐々木 佑梨</div>
+									<ul class="cell">
+										<li>K013C1296</li>
+										<li>佐々木 佑梨</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div id="student2" class="panel-collapse collapse">
+							<div class="panel-body text-center">
+								<button type="button" class="btn btn-primary">出席</button>
+								<button type="button" class="btn btn-warning">遅刻</button>
+								<button type="button" class="btn btn-danger">欠席</button>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<div class="panel-title h4">
+								<div data-toggle="collapse" data-parent="#accordion" href="#student3">
+									<div class="cell create-circle"></div>
+									<div class="cell">K013C1296 佐々木 佑梨</div>
+									<ul class="cell">
+										<li>K013C1296</li>
+										<li>佐々木 佑梨</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div id="student3" class="panel-collapse collapse">
+							<div class="panel-body text-center">
+								<button type="button" class="btn btn-primary">出席</button>
+								<button type="button" class="btn btn-warning">遅刻</button>
+								<button type="button" class="btn btn-danger">欠席</button>
+							</div>
 						</div>
 					</div>
 				</div>
