7fe2cdf630b28fdc74f1cf4a1fb292481cb5d36d
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-11-29 03:58:12 +0900

    issue#283 学外説明会系画面のタイトルを修正

--- fuel/app/classes/controller/recruit/offcampus.php
+++ fuel/app/classes/controller/recruit/offcampus.php
@@ -4,31 +4,31 @@ class Controller_Recruit_Offcampus extends Controller_Loggedin
 {
 	public function action_add()
 	{
-		$this->template->title = '学内説明会追加';
+		$this->template->title = '学外説明会追加';
 		$this->template->content = View::forge('recruit/off_campus_add');
 	}
 
 	public function action_edit()
 	{
-		$this->template->title = '学内説明会編集';
+		$this->template->title = '学外説明会編集';
 		$this->template->content = View::forge('recruit/off_campus_edit');
 	}
 
 	public function action_list()
 	{
-		$this->template->title = '学内説明会一覧';
+		$this->template->title = '学外説明会一覧';
 		$this->template->content = View::forge('recruit/off_campus_list');
 	}
 
 	public function action_confirm()
 	{
-		$this->template->title = '学内説明会確認';
+		$this->template->title = '学外説明会確認';
 		$this->template->content = View::forge('recruit/off_campus_confirm');
 	}
 
 	public function action_detail()
 	{
-		$this->template->title = '学内説明会詳細';
+		$this->template->title = '学外説明会詳細';
 		$this->template->content = View::forge('recruit/off_campus_detail');
 	}
 }
