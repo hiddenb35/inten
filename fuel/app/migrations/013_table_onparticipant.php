88743161ffcb800640963975dd86f101a8003311
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-11-29 15:45:23 +0900

    issue#285 学内説明会参加者テーブルの申し込んだ日のカラム名を変更

--- fuel/app/migrations/013_table_onparticipant.php
+++ fuel/app/migrations/013_table_onparticipant.php
@@ -10,7 +10,7 @@ class Table_onparticipant
 			'id'          => array('type' => 'int', 'auto_increment' => true, 'unsigned' => true, 'comment' => '学内説明会参加ID'),
 			'oncampus_id' => array('type' => 'int', 'unsigned' => true, 'comment' => '学内説明会ID'),
 			'student_id'  => array('type' => 'int', 'unsigned' => true, 'comment' => '生徒ID'),
-			'entry_date'  => array('type' => 'int', 'comment' => '申し込んだ日'),
+			'entry_at'  => array('type' => 'int', 'comment' => '申し込んだ日'),
 		), array('id'), true, false, null, array(
 			array(
 				'key'       => 'oncampus_id',
