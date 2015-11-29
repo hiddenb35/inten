9afdb33d6599eba3a0b26623628d59c3a17abca6
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-11-29 19:05:09 +0900

    issue#286 学外説明会モデルにvalidateメソッドを作成

--- fuel/app/classes/model/offcampus.php
+++ fuel/app/classes/model/offcampus.php
@@ -108,4 +108,30 @@ class Model_Offcampus extends \Orm\Model
 			'cascade_delete' => false,
 		)
 	);
+
+	public static function validate()
+	{
+		$val = Validation::forge();
+		$val->add_callable('exvalidation');
+		$val->add_field('company_name', '企業名', 'trim|required|max_length[128]');
+		$val->add_field('company_code', '企業コード', 'trim|required|max_length[64]');
+		$val->add_field('start_date', '開催日', 'trim|required')->add_rule('valid_date', 'Y/m/d');
+		$val->add_field('start_time', '開始時間', 'trim|required')->add_rule('valid_date', 'H:i:s');
+		$val->add_field('end_time', '終了時間', 'trim|required')->add_rule('valid_date', 'H:i:s');
+		$val->add_field('entry_start', '申込期限(開始)', 'trim|required')->add_rule('valid_data', 'Y/m/d');
+		$val->add_field('entry_end', '申込期限(終了)', 'trim|required')->add_rule('valid_date', 'Y/m/d');
+		$val->add_field('target', '対象者', 'trim');
+		$val->add_field('location', '開催場所', 'trim|required');
+		$val->add_field('content', '内容', 'trim');
+		$val->add_field('explainer', '説明者', 'trim');
+		$val->add_field('bring', '持ち物', 'trim');
+		$val->add_field('url', 'URL', 'trim|valid_url');
+		$val->add_field('entry_method', '申し込み方法', 'trim');
+		$val->add_field('tel', '電話番号', 'trim');
+		$val->add_field('email', 'メールアドレス', 'trim|valid_email');
+		$val->add_field('recruitment','募集職種', 'valid_array');
+		$val->add_field('files', '添付ファイル', 'valid_array');
+		$val->add_field('note', '備考', 'trim');
+		return $val;
+	}
 }
