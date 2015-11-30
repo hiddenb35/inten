4489e355f99deccfc33ab15fc37d99c7c67080e0
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-11-30 23:10:25 +0900

    issue#288 AuthコントローラにコメントとPHPDocを追加

--- fuel/app/classes/controller/auth.php
+++ fuel/app/classes/controller/auth.php
@@ -6,8 +6,12 @@ class Controller_Auth extends Controller_Template
 	const TEACHER_AUTHENTICATION_NAME = 'teacherauth';
 	public $template = 'template/before_login/template';
 
+	/**
+	 * 学生ログイン
+	 */
 	public function action_slogin()
 	{
+		/* ログイン処理 */
 		if(Input::is_post())
 		{
 			$auth = Auth::instance(self::STUDENT_AUTHENTICATION_NAME);
@@ -17,12 +21,17 @@ class Controller_Auth extends Controller_Template
 			($auth->login()) ? Response::redirect('/') : Response::redirect('auth/slogin');
 		}
 
+		/* 学生用ログイン画面を表示 */
 		$this->template->title = '生徒用ログインページ';
 		$this->template->content = View::forge('auth/student_login');
 	}
 
+	/**
+	 * 教員ログイン
+	 */
 	public function action_tlogin()
 	{
+		/* ログイン処理 */
 		if(Input::is_post())
 		{
 			$auth = Auth::instance(self::TEACHER_AUTHENTICATION_NAME);
@@ -32,10 +41,16 @@ class Controller_Auth extends Controller_Template
 			($auth->login()) ? Response::redirect('/') : Response::redirect('auth/tlogin');
 		}
 
+		/* 教員用ログインを表示 */
 		$this->template->title = '教員用ログインページ';
 		$this->template->content = View::forge('auth/teacher_login');
 	}
 
+	/**
+	 * ログアウト
+	 * @throws HttpNotFoundException
+	 * @throws HttpServerErrorException
+	 */
 	public function action_logout()
 	{
 		if(Auth::check())
@@ -61,6 +76,9 @@ class Controller_Auth extends Controller_Template
 		}
 	}
 
+	/**
+	 * 404
+	 */
 	public function action_404()
 	{
 		$this->response_status = 404;
