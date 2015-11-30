2e1859c4492931790242ae56cc45f0c1a6e10757
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-11-30 23:33:10 +0900

    issue#288 Loggedinコントローラのビューファイルの定数化と汎用化

--- fuel/app/classes/controller/loggedin.php
+++ fuel/app/classes/controller/loggedin.php
@@ -2,6 +2,14 @@
 
 class Controller_Loggedin extends Controller_Hybrid
 {
+	const HEADER_ADMIN_VIEW = 'template/after_login/header_admin';
+	const HEADER_TEACHER_VIEW = 'template/after_login/header_teacher';
+	const HEADER_STUDENT_VIEW = 'template/after_login/header_student';
+	const SIDEBAR_ADMIN_VIEW = 'template/after_login/sidebar_admin';
+	const SIDEBAR_TEACHER_VIEW = 'template/after_login/sidebar_teacher';
+	const SIDEBAR_STUDENT_VIEW = 'template/after_login/sidebar_student';
+	const FOOTER_COMMON_VIEW = 'template/after_login/footer';
+
 	/**
 	 * @var string|\View
 	 */
@@ -39,24 +47,24 @@ class Controller_Loggedin extends Controller_Hybrid
 			/* ログインしているユーザ別にテンプレートを設定 */
 			if ($this->is_admin())
 			{
-				$this->template->header = View::forge('template/after_login/header_admin');
-				$this->template->sidebar = View::forge('template/after_login/sidebar_admin');
+				$this->template->header = View::forge(self::HEADER_ADMIN_VIEW);
+				$this->template->sidebar = View::forge(self::SIDEBAR_ADMIN_VIEW);
 				$user_info['body_skin'] = 'skin-yellow';
 
 			}
 			elseif ($this->is_teacher())
 			{
-				$this->template->header = View::forge('template/after_login/header_teacher');
-				$this->template->sidebar = View::forge('template/after_login/sidebar_teacher');
+				$this->template->header = View::forge(self::HEADER_TEACHER_VIEW);
+				$this->template->sidebar = View::forge(self::SIDEBAR_TEACHER_VIEW);
 				$user_info['body_skin'] = 'skin-red';
 			}
 			elseif ($this->is_student())
 			{
-				$this->template->header = View::forge('template/after_login/header_student');
-				$this->template->sidebar = View::forge('template/after_login/sidebar_student');
+				$this->template->header = View::forge(self::HEADER_STUDENT_VIEW);
+				$this->template->sidebar = View::forge(self::SIDEBAR_STUDENT_VIEW);
 			}
 
-			$this->template->footer  = View::forge('template/after_login/footer');
+			$this->template->footer  = View::forge(self::FOOTER_COMMON_VIEW);
 			$this->template->set_global('user_info', $user_info);
 		}
 	}
