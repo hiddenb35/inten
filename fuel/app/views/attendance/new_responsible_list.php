5742ed087acd2f7e1f5e5e7399c6afb9184b3a76
Shota Narumi <k013c1112@it-neec.jp>
2015-12-01 13:53:16 +0900

    issue#290 新担当するクラス一覧画面の作成

---
+++ fuel/app/views/attendance/new_responsible_list.php
@@ -0,0 +1,47 @@
+<div id="NEW_RESPONSIBLE_LIST">
+	<section class="content-header">
+		<h1>
+			担当するクラスの授業一覧
+		</h1>
+		<ol class="breadcrumb">
+			<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+		</ol>
+	</section>
+	<!-- Main content -->
+	<section class="content">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm-6 col-md-4 col-lg-3">
+					<div class="box text-center">
+						<div class="box-body">
+							<div class="class_name h2">IS07-1</div>
+							<div class="college_name">ITカレッジ</div>
+							<div class="department_name">ITスペシャリスト科</div>
+							<div class="class_member">人数：45人</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4 col-lg-3">
+					<div class="box text-center">
+						<div class="box-body">
+							<div class="class_name h2">IS07-2</div>
+							<div class="college_name">ITカレッジ</div>
+							<div class="department_name">ITスペシャリスト科</div>
+							<div class="class_member">人数：40人</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4 col-lg-3">
+					<div class="box text-center">
+						<div class="box-body">
+							<div class="class_name h2">IS09-1</div>
+							<div class="college_name">ITカレッジ</div>
+							<div class="department_name">ITスペシャリスト科</div>
+							<div class="class_member">人数：50人</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
