05d609cb95ede9806f826a8a9c2aed832e5cf794
kasai keita <kasai@example.local>
2015-12-03 13:39:08 +0900

    issue#297 自分の担当するクラス一覧画面の動的表示するようにしました。

--- fuel/app/views/attendance/new_responsible_list.php
+++ fuel/app/views/attendance/new_responsible_list.php
@@ -11,36 +11,19 @@
 	<section class="content">
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-sm-6 col-md-4 col-lg-3">
-					<div class="box text-center">
-						<div class="box-body">
-							<div class="class_name h2">IS07-1</div>
-							<div class="college_name">ITカレッジ</div>
-							<div class="department_name">ITスペシャリスト科</div>
-							<div class="class_member">人数：45人</div>
+				<?php foreach($class_lists as $class_list): ?>
+					<div class="col-sm-6 col-md-4 col-lg-3">
+						<div class="box text-center">
+							<a href="<?php echo $class_list['link_url']; ?>">
+							<div class="box-body">
+								<div class="class_name h2"><?php echo $class_list['name']; ?></div>
+								<div class="college_name"><?php echo $class_list['college_name']; ?></div>
+								<div class="department_name"><?php echo $class_list['course_name']; ?></div>
+								<div class="class_member">人数：<?php echo $class_list['student_sum']; ?>名</div>
+							</div>
 						</div>
 					</div>
-				</div>
-				<div class="col-sm-6 col-md-4 col-lg-3">
-					<div class="box text-center">
-						<div class="box-body">
-							<div class="class_name h2">IS07-2</div>
-							<div class="college_name">ITカレッジ</div>
-							<div class="department_name">ITスペシャリスト科</div>
-							<div class="class_member">人数：40人</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-sm-6 col-md-4 col-lg-3">
-					<div class="box text-center">
-						<div class="box-body">
-							<div class="class_name h2">IS09-1</div>
-							<div class="college_name">ITカレッジ</div>
-							<div class="department_name">ITスペシャリスト科</div>
-							<div class="class_member">人数：50人</div>
-						</div>
-					</div>
-				</div>
+				<?php endforeach; ?>
 			</div>
 		</div>
 	</section>
