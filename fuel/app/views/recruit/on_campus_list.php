29a7e60f361b3f151da6b4cf69f8144915308de1
Shota Narumi <k013c1112@it-neec.jp>
2015-12-05 17:32:51 +0900

    issue#300 学内説明会一覧画面の作成

---
+++ fuel/app/views/recruit/on_campus_list.php
@@ -0,0 +1,76 @@
+<div id="ON_CAMPUS_LIST">
+	<section class="content-header">
+		<h1>
+			学内説明会一覧画面
+		</h1>
+		<ol class="breadcrumb">
+			<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+		</ol>
+	</section>
+	<!-- Main content -->
+	<section class="content">
+		<div class="container-fluid">
+			<div class="hd-btn text-right">
+				<a href="#" class="btn btn-primary">締め切りログ</a>
+			</div>
+			<div class="box">
+				<div class="box-header text-center outer-box-header clearfix">
+					<a href="#" class="btn btn-primary pull-right">締め切りログ</a>
+					<div class="row pull-left">
+						<div class="tab col-xs-3">新着(昇順)</div>
+						<div class="tab col-xs-3">新着(降順)</div>
+						<div class="tab col-xs-3">締め切り(昇順)</div>
+						<div class="tab col-xs-3">締め切り(降順)</div>
+					</div>
+				</div>
+				<div class="outer-box-body">
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="box inner-box">
+								<div class="box-header inner-box-header">第一商事</div>
+								<div class="box-body inner-box-body clearfix">
+									<div class="left-in-box pull-left">
+										<div>業種 システムエンジニア</div>
+										<div>締め切り 2016/11/30</div>
+									</div>
+									<div class="right-in-box pull-right">
+										<a href="#" class="btn btn-primary">詳細</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="box inner-box">
+								<div class="box-header inner-box-header">ABC商事</div>
+								<div class="box-body inner-box-body clearfix">
+									<div class="left-in-box pull-left">
+										<div>業種 システムエンジニア</div>
+										<div>締め切り 2016/11/30</div>
+									</div>
+									<div class="right-in-box pull-right">
+										<a href="#" class="btn btn-primary">詳細</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="box inner-box">
+								<div class="box-header inner-box-header">XX株式会社</div>
+								<div class="box-body inner-box-body clearfix">
+									<div class="left-in-box pull-left">
+										<div>業種 システムエンジニア</div>
+										<div>締め切り 2016/11/30</div>
+									</div>
+									<div class="right-in-box pull-right">
+										<a href="#" class="btn btn-primary">詳細</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+</div>
+</section>
+</div>
