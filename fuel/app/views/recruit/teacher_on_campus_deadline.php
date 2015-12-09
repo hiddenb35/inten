0c79c46f2f4a515a1e097ff2ed1afcbab8629ae8
Shota Narumi <k013c1112@it-neec.jp>
2015-12-10 03:09:09 +0900

    issue306 教員用学内説明会一覧画面締め切りログの作成

---
+++ fuel/app/views/recruit/teacher_on_campus_deadline.php
@@ -0,0 +1,101 @@
+<div id="TEACHER_ON_CAMPUS_DEADLINE">
+	<section class="content-header">
+		<h1>
+			学内説明会一覧画面
+		</h1>
+		<ol class="breadcrumb">
+			<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+		</ol>
+	</section>
+	<!-- Main content -->
+	<section class="content">
+		<div class="container-fluid">
+			<div class="header-title clearfix">
+				<span class="inner-page-title pull-left">締め切りログ</span>
+				<span class="page-count pull-right">76件中 1-5件</span>
+			</div>
+			<div class="outer-box-body">
+				<div class="row">
+					<div class="col-sm-6">
+						<a href="#">
+							<div class="box inner-box">
+								<div class="box-header inner-box-header clearfix">第一商事</div>
+								<div class="box-body inner-box-body clearfix">
+									<div class="left-in-box pull-left left-box-color">
+										<div>
+											<span class="inner-title">業種 </span>
+											<span class="inner-content">システムエンジニア</span>
+										</div>
+										<div>
+											<span class="inner-title">締め切り </span>
+											<span class="inner-content">2016/11/30</span>
+										</div>
+									</div>
+									<div class="right-in-box pull-right">
+										<a href="#" class="btn btn-danger">詳細</a>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-sm-6">
+						<a href="#">
+							<div class="box inner-box">
+								<div class="box-header inner-box-header clearfix">第一商事</div>
+								<div class="box-body inner-box-body clearfix">
+									<div class="left-in-box pull-left left-box-color">
+										<div>
+											<span class="inner-title">業種 </span>
+											<span class="inner-content">システムエンジニア</span>
+										</div>
+										<div>
+											<span class="inner-title">締め切り </span>
+											<span class="inner-content">2016/11/30</span>
+										</div>
+									</div>
+									<div class="right-in-box pull-right">
+										<a href="#" class="btn btn-danger">詳細</a>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-sm-6">
+						<a href="#">
+							<div class="box inner-box">
+								<div class="box-header inner-box-header clearfix">第一商事</div>
+								<div class="box-body inner-box-body clearfix">
+									<div class="left-in-box pull-left left-box-color">
+										<div>
+											<span class="inner-title">業種 </span>
+											<span class="inner-content">システムエンジニア</span>
+										</div>
+										<div>
+											<span class="inner-title">締め切り </span>
+											<span class="inner-content">2016/11/30</span>
+										</div>
+									</div>
+									<div class="right-in-box pull-right">
+										<a href="#" class="btn btn-danger">詳細</a>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+			<nav>
+				<ul class="pagination">
+					<li><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a>
+					</li>
+					<li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
+					<li><a href="#">2<span aria-hidden="true"></span></a></li>
+					<li><a href="#">3<span aria-hidden="true"></span></a></li>
+					<li><a href="#">4<span aria-hidden="true"></span></a></li>
+					<li><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</section>
+</div>
