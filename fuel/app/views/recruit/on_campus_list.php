d5d2c6b72ac5ac34b1b72b4e15b968902d00d993
Shota Narumi <k013c1112@it-neec.jp>
2015-12-11 09:41:17 +0900

    issue#310 学内説明会一覧画面の修正
    ・不要なdivの削除

--- fuel/app/views/recruit/on_campus_list.php
+++ fuel/app/views/recruit/on_campus_list.php
@@ -16,6 +16,7 @@
 			<div class="box">
 				<div class="box-header text-center outer-box-header clearfix">
 					<a href="#" class="btn btn-primary pull-right">締め切りログ</a>
+
 					<div class="row pull-left button-box">
 						<a href="#" class="btn btn-default tab col-xs-12 col-sm-3">新着
 							<i class="fa fa-fw fa-arrow-up"></i>
@@ -38,8 +39,10 @@
 								<div class="box-header inner-box-header">第一商事</div>
 								<div class="box-body inner-box-body clearfix">
 									<div class="left-in-box pull-left">
-										<div><span class="inner-title">業種 </span><span class="inner-content">システムエンジニア</span></div>
-										<div><span class="inner-title">締め切り </span><span class="inner-content">2016/11/30</span></div>
+										<div><span class="inner-title">業種 </span><span
+												class="inner-content">システムエンジニア</span></div>
+										<div><span class="inner-title">締め切り </span><span class="inner-content">2016/11/30</span>
+										</div>
 									</div>
 									<div class="right-in-box pull-right">
 										<a href="#" class="btn btn-primary">詳細</a>
@@ -52,8 +55,10 @@
 								<div class="box-header inner-box-header">ABC商事</div>
 								<div class="box-body inner-box-body clearfix">
 									<div class="left-in-box pull-left">
-										<div><span class="inner-title">業種 </span><span class="inner-content">システムエンジニア</span></div>
-										<div><span class="inner-title">締め切り </span><span class="inner-content">2016/11/30</span></div>
+										<div><span class="inner-title">業種 </span><span
+												class="inner-content">システムエンジニア</span></div>
+										<div><span class="inner-title">締め切り </span><span class="inner-content">2016/11/30</span>
+										</div>
 									</div>
 									<div class="right-in-box pull-right">
 										<a href="#" class="btn btn-primary">詳細</a>
@@ -66,8 +71,10 @@
 								<div class="box-header inner-box-header">XX株式会社</div>
 								<div class="box-body inner-box-body clearfix">
 									<div class="left-in-box pull-left">
-										<div><span class="inner-title">業種 </span><span class="inner-content">システムエンジニア</span></div>
-										<div><span class="inner-title">締め切り </span><span class="inner-content">2016/11/30</span></div>
+										<div><span class="inner-title">業種 </span><span
+												class="inner-content">システムエンジニア</span></div>
+										<div><span class="inner-title">締め切り </span><span class="inner-content">2016/11/30</span>
+										</div>
 									</div>
 									<div class="right-in-box pull-right">
 										<a href="#" class="btn btn-primary">詳細</a>
@@ -79,6 +86,5 @@
 				</div>
 			</div>
 		</div>
-</div>
-</section>
+	</section>
 </div>
