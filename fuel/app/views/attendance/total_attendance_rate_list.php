6bde9ecc37d590dd74e2ad65b9b61d612e4029dc
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-12-12 19:27:54 +0900

    issue#320 出席系ビューを最新のテンプレートに適合

--- fuel/app/views/attendance/total_attendance_rate_list.php
+++ fuel/app/views/attendance/total_attendance_rate_list.php
@@ -1,59 +1,49 @@
-<div id="TOTAL_ATTENDANCE_RATE_LIST">
-	<section class="content-header">
-		<h1>
-			担当する授業の出席率一覧画面
-		</h1>
-		<ol class="breadcrumb">
-			<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-		</ol>
-	</section>
-	<!-- Main content -->
-	<section class="content">
-		<div class="container-fluid">
-			<div class="row">
-				<div class="table-responsive">
-					<table class="table table-bordered table-striped table-hover table-condensed">
-						<thead>
-						<tr>
-							<th class="col-lg-3 col-md-3 col-sm-3">学籍番号</th>
-							<th class="col-lg-3 col-md-3 col-sm-3">名前</th>
-							<th class="col-lg-3 col-md-3 col-sm-3 hidden-xs">フリガナ</th>
-							<th colspan="2" class="col-lg-2 col-md-2 col-sm-2">出席率</th>
-						</tr>
-						</thead>
-						<tbody class="row">
-						<tr>
-							<td>K013C1296</td>
-							<td>加藤拓磨</td>
-							<td class="hidden-xs">カトウタクマ</td>
-							<td class="hidden-xs col-md-11 col-sm-11 hidden-xs progress progress-xs progress-striped active"><div class="progress-bar progress-bar-primary" style="width: 100%;"></div></td>
-							<td class="badge bg-aqua">100%</td>
-						</tr>
-						<tr>
-							<td>K013C1296</td>
-							<td>加藤拓磨</td>
-							<td class="hidden-xs">カトウタクマ</td>
-							<td class="hidden-xs progress progress-xs progress-striped active"><div class="progress-bar progress-bar-primary" style="width: 75%;"></div></td>
-							<td class="badge bg-aqua">75%</td>
-						</tr>
-						<tr>
-							<td>K013C1296</td>
-							<td>加藤拓磨</td>
-							<td class="hidden-xs">カトウタクマ</td>
-							<td class="hidden-xs progress progress-xs progress-striped active"><div class="progress-bar progress-bar-danger" style="width: 74%;"></div></td>
-							<td class="badge bg-red">74%</td>
-						</tr>
-						<tr>
-							<td>K013C1296</td>
-							<td>加藤拓磨</td>
-							<td class="hidden-xs">カトウタクマ</td>
-							<td class="hidden-xs progress progress-xs progress-striped active"><div class="progress-bar progress-bar-danger" style="width: 5%;"></div></td>
-							<td class="badge bg-red">5%</td>
-						</tr>
-						</tbody>
-					</table>
-				</div>
+<!-- Main content -->
+<section id="TOTAL_ATTENDANCE_RATE_LIST" class="content">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="table-responsive">
+				<table class="table table-bordered table-striped table-hover table-condensed">
+					<thead>
+					<tr>
+						<th class="col-lg-3 col-md-3 col-sm-3">学籍番号</th>
+						<th class="col-lg-3 col-md-3 col-sm-3">名前</th>
+						<th class="col-lg-3 col-md-3 col-sm-3 hidden-xs">フリガナ</th>
+						<th colspan="2" class="col-lg-2 col-md-2 col-sm-2">出席率</th>
+					</tr>
+					</thead>
+					<tbody class="row">
+					<tr>
+						<td>K013C1296</td>
+						<td>加藤拓磨</td>
+						<td class="hidden-xs">カトウタクマ</td>
+						<td class="hidden-xs col-md-11 col-sm-11 hidden-xs progress progress-xs progress-striped active"><div class="progress-bar progress-bar-primary" style="width: 100%;"></div></td>
+						<td class="badge bg-aqua">100%</td>
+					</tr>
+					<tr>
+						<td>K013C1296</td>
+						<td>加藤拓磨</td>
+						<td class="hidden-xs">カトウタクマ</td>
+						<td class="hidden-xs progress progress-xs progress-striped active"><div class="progress-bar progress-bar-primary" style="width: 75%;"></div></td>
+						<td class="badge bg-aqua">75%</td>
+					</tr>
+					<tr>
+						<td>K013C1296</td>
+						<td>加藤拓磨</td>
+						<td class="hidden-xs">カトウタクマ</td>
+						<td class="hidden-xs progress progress-xs progress-striped active"><div class="progress-bar progress-bar-danger" style="width: 74%;"></div></td>
+						<td class="badge bg-red">74%</td>
+					</tr>
+					<tr>
+						<td>K013C1296</td>
+						<td>加藤拓磨</td>
+						<td class="hidden-xs">カトウタクマ</td>
+						<td class="hidden-xs progress progress-xs progress-striped active"><div class="progress-bar progress-bar-danger" style="width: 5%;"></div></td>
+						<td class="badge bg-red">5%</td>
+					</tr>
+					</tbody>
+				</table>
 			</div>
 		</div>
-	</section>
-</div>
+	</div>
+</section>
