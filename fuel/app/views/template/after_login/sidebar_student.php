867464b4f945075d087e5b2af636f4e9cdbc802c
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-12-12 17:07:35 +0900

    issue#319 生徒用サイドバーの更新

--- fuel/app/views/template/after_login/sidebar_student.php
+++ fuel/app/views/template/after_login/sidebar_student.php
@@ -8,6 +8,16 @@
 				<i class="fa ion-clock"></i> <span>時間割表示</span>
 			</a>
 		</li>
+		<li class="treeview">
+			<a href="/recruit/oncampus/list">
+				<i class="fa ion-clock"></i> <span>学内説明会情報</span>
+			</a>
+		</li>
+		<li class="treeview">
+			<a href="/recruit/offcampus/list">
+				<i class="fa ion-clock"></i> <span>学外説明会情報</span>
+			</a>
+		</li>
 		<li>
 			<a href="/setting/pass_update">
 				<i class="fa ion-settings"></i> <span>パスワード変更</span>
