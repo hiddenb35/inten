53271f97e2f012203dc07eec8dcbcad13d2ac522
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-12-12 18:08:23 +0900

    issue#320 パンくずリスト用のBreadcrumbクラスを作成

---
+++ fuel/app/classes/breadcrumb.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * パンくずリストを生成
+ */
+class Breadcrumb
+{
+	/**
+	 * パンくずの配列
+	 * @var array
+	 */
+	private $items = array();
+
+
+	/**
+	 * パンくずを追加
+	 * @param $name
+	 * @param null $url
+	 * @param null $icon
+	 */
+	public function add_item($name, $url = null, $icon = null)
+	{
+		array_push($this->items, array(
+			'name' => $name,
+			'url'  => $url,
+			'icon' => $icon,
+		));
+	}
+
+	/**
+	 * パンくずを配列で追加
+	 */
+	public function add_items()
+	{
+		foreach(func_get_args() as $arg)
+		{
+			$this->add_item($arg[0], $arg[1], $arg[2]);
+		}
+	}
+
+	/**
+	 * パンくずリストを出力
+	 */
+	public function render()
+	{
+		$last_index = count($this->items) - 1;
+
+		foreach($this->items as $idx => $item)
+		{
+			$icon = (isset($item['icon'])) ? sprintf('<i class="fa %s"></i>%s ', $item['icon'], $item['name']) : $item['name'];
+			$url = (isset($item['url'])) ? sprintf('<a href="%s">%s</a>', $item['url'], $icon) : $icon;
+			$li = ($last_index === $idx) ? '<li class="active">' . $url . '</li>' : '<li>' . $url . '</li>';
+			echo $li, "\n";
+		}
+	}
+}
