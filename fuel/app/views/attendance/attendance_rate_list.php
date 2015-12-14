a7926ece74d848eb7c730d483cc5faf05beb6a24
Shota Narumi <k013c1112@it-neec.jp>
2015-12-14 16:16:42 +0900

    issue#330 出席率一覧画面の修正2
    ・htmlの修正2

--- fuel/app/views/attendance/attendance_rate_list.php
+++ fuel/app/views/attendance/attendance_rate_list.php
@@ -9,8 +9,7 @@
 						<th class="col-sm-3 col-xs-5">学籍番号</th>
 						<th class="col-sm-3 col-xs-5">名前</th>
 						<th class="col-sm-3 hidden-xs">フリガナ</th>
-						<th class="col-xs-2 visible-xs">出席率</th>
-						<th colspan="2" class="col-sm-3 hidden-xs">出席率</th>
+						<th colspan="2" class="col-sm-3 col-xs-2">出席率</th>
 					</tr>
 					</thead>
 					<tbody>
@@ -19,11 +18,10 @@
 							<td><?php echo $student_list['number']; ?></td>
 							<td><?php echo $student_list['full_name']; ?></td>
 							<td class="hidden-xs"><?php echo $student_list['full_name_kana']; ?></td>
-							<td class="visible-xs text-center"><span class="badge <?php echo $student_list['rate_bg_class']; ?>"><?php echo $student_list['rate']; ?>></span></td>
 							<td class="hidden-xs progress">
 								<div class="progress-bar <?php echo $student_list['rate_bar_class']; ?>" style="width: <?php echo $student_list['rate']; ?>;"></div>
 							</td>
-							<td class="hidden-xs"><span class="badge <?php echo $student_list['rate_bg_class']; ?>"><?php echo $student_list['rate']; ?></span></td>
+							<td class="text-center"><span class="badge <?php echo $student_list['rate_bg_class']; ?>"><?php echo $student_list['rate']; ?></span></td>
 						</tr>
 					<?php endforeach; ?>
 					</tbody>
