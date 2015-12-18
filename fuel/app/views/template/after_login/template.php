45a4915bb12ab228a9028eec46888682ae5a7d78
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-12-18 19:12:52 +0900

    issue#340 common.cssを作成し、全画面で共通のCSSを移動

--- fuel/app/views/template/after_login/template.php
+++ fuel/app/views/template/after_login/template.php
@@ -37,6 +37,7 @@
 
 		<!--  -->
 		<?php echo Asset::css('after-login.css'); ?>
+		<?php echo Asset::css('common.css'); ?>
 
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
