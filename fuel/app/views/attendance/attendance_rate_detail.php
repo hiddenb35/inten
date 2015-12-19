cfc221bc366c9883665ee2338210045e9839387b
Keita Kasai <k013c1345@it-neec.jp>
2015-12-19 19:12:44 +0000

    issue#341 出席率詳細画面のhtmlの修正をしました。

--- fuel/app/views/attendance/attendance_rate_detail.php
+++ fuel/app/views/attendance/attendance_rate_detail.php
@@ -1,86 +1,46 @@
 <!-- Main content -->
-<section id="ATTENDANCE_RATE_DETAIL" class="content clearfix">
-<div class="container-fluid">
+<section id="ATTENDANCE_RATE_DETAIL" class="content">
+    <div class="">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="center-block student-table">
+                    <div class="student-table-row">
+                        <div class="student-table-cell"><h3>学籍番号</h3></div>
+                        <div class="student-table-cell"><h3>:K013C1296</h3></div>
+                    </div>
+                    <div class="student-table-row">
+                        <div class="student-table-cell"><h3>名前</h3></div>
+                        <div class="student-table-cell"><h3>:佐々木aaaaa</h3></div>
+                    </div>
+                </div>
 
-    <div class="student-number">
-        <h2 class="number"><span>学籍番号</span>:K013C1296</h2>
-        <h2 class="name"><span>名前</span>:佐々木</h2>
-    </div>
+                <div class="text-center">
+                    <h3 class=""><span class="glyphicon glyphicon-star" aria-hidden="true">ビジネスマナー</span></h3>
+                </div>
+                <div class="col-sm-6 hidden-xs"></div>
+                <div class="col-sm-6">
+                    <table class="table">
+                        <tr class="bg-primary"><th>授業数</th><th>不足</th><th>出席率</th></tr>
+                        <tr><td>15</td><td>0</td><td>80.0％</td></tr>
+                        <tr class="bg-primary"><th>出席</th><th>欠席</th><th>遅刻</th></tr>
+                        <tr><td>12</td><td>3</td><td>0</td></tr>
+                    </table>
+                </div>
 
-    <div>
-        <h2 class="mainsubject"><span class="glyphicon glyphicon-star" aria-hidden="true">ビジネスマナー(１年前期)</span></h2>
-    </div>
+                <div class="attendance-log">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <tr class="bg-primary"><th></th><th>1時限</th><th>2時限</th><th>3時限</th><th>4時限</th><th>5時限</th><th>6時限</th></tr>
+                            <tr><th>4/1</th><td>出席</td><td>出席</td><td>出席</td><td>出席</td><td>出席</td><td>欠席</td></tr>
+                            <tr><th>4/1</th><td>出席</td><td>出席</td><td>出席</td><td>出席</td><td>出席</td><td>欠席</td></tr>
+                            <tr><th>4/1</th><td>出席</td><td>出席</td><td>出席</td><td>出席</td><td>出席</td><td>欠席</td></tr>
+                            <tr><th>4/1</th><td>出席</td><td>出席</td><td>出席</td><td>出席</td><td>出席</td><td>欠席</td></tr>
+                        </table>
+                    </div>
+                </div>
 
-    <div class="row detail">
-        <div class="pie col-lg-offset-1 col-md-offset-1 col-lg-5 col-md-5">
+                <button type="button" class="btn btn-primary pull-right">戻る</button>
+            </div>
         </div>
-        <div class="col-lg-5 col-md-5">
-            <table class="table table-bordered table-striped table-hover table-condensed">
-                <thead>
-                    <tr>
-                        <th>授業数</th>
-                        <th>不足</th>
-                        <th>出席率</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>15</td>
-                        <td>0</td>
-                        <td>80.0%</td>
-                    <tr>
-                </tbody>
-                <thead>
-                    <tr>
-                        <th>出席</th>
-                        <th>欠席</th>
-                        <th>遅刻</th>
-                    <tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>12</td>
-                        <td>3</td>
-                        <td>0</td>
-                    <tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-
-    <div class="row">
-        <table class="table table-bordered table-striped table-hover table-condensed">
-            <thead>
-                <tr>
-                    <th></th>
-                    <th>1限目</th>
-                    <th>2限目</th>
-                    <th>3限目</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>4/1</td>
-                    <td>出席</td>
-                    <td>出席</td>
-                    <td>出席</td>
-                </tr>
-                <tr>
-                    <td>4/1</td>
-                    <td>出席</td>
-                    <td>出席</td>
-                    <td>出席</td>
-                </tr>
-                <tr>
-                    <td>4/1</td>
-                    <td>出席</td>
-                    <td>出席</td>
-                    <td>出席</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="row">
-    <button type="button" class="button col-md-offset-9 col-md-2 col-xs-offset-8 col-xs-3 btn btn-primary btn-lg">戻る</button>
     </div>
 </section>
