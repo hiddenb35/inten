b114c12083f02f22b4f600fd4d162db290ef0d46
Yuki Watanabe <ywatanabeznzt@gmail.com>
2015-12-20 04:08:33 +0900

    issue#342 サンプル用のインデックスを作成

--- fuel/app/classes/controller/sample.php
+++ fuel/app/classes/controller/sample.php
@@ -4,7 +4,12 @@ class Controller_Sample extends Controller_Loggedin
 {
 	public function action_index()
 	{
-
+		static $samples = array(
+			'グラフ' => 'graph',
+		);
+		$this->template->title = 'サンプル一覧';
+		$this->template->content = View::forge('sample/index');
+		$this->template->content->set('samples', $samples);
 	}
 
 	public function action_graph()
