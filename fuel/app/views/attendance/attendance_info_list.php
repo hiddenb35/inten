4b3e14de0b568b32325e26a9089fc50d1da43132
Keita Kasai <k013c1345@it-neec.jp>
2015-12-19 18:16:05 +0000

    issue#338 css適用に伴うhtmlの修正

--- fuel/app/views/attendance/attendance_info_list.php
+++ fuel/app/views/attendance/attendance_info_list.php
@@ -3,14 +3,14 @@
 	<div class="">
 		<div class="row">
 			<div class="col-md-10 col-md-offset-1">
-				<div class="center-block" style="display:table;">
-					<div style="display: table-row">
-						<div style="display: table-cell;"><h3>学籍番号</h3></div>
-						<div style="display: table-cell;"><h3>:K013C1296</h3></div>
+				<div class="center-block student-table">
+					<div class="student-table-row">
+						<div class="student-table-cell"><h3>学籍番号</h3></div>
+						<div class="student-table-cell"><h3>:K013C1296</h3></div>
 					</div>
-					<div style="display: table-row">
-						<div style="display: table-cell;"><h3>名前</h3></div>
-						<div style="display: table-cell;"><h3>:佐々木aaaaa</h3></div>
+					<div class="student-table-row">
+						<div class="student-table-cell"><h3>名前</h3></div>
+						<div class="student-table-cell"><h3>:佐々木aaaaa</h3></div>
 					</div>
 				</div>
 
@@ -36,19 +36,19 @@
 
 				<div class="info-box">
 					<div class="text-center">
-						<h3 class="mainitem"><span class="glyphicon glyphicon-star" aria-hidden="true">全体</span></h3>
+						<h3 class=""><span class="glyphicon glyphicon-star" aria-hidden="true">全体</span></h3>
 					</div>
-					<div class="row">
-						<div class="col-sm-6">
+					<div class="row all-attendance-rate">
+						<div class="col-sm-6 all-attendance-rate-col">
 							<table class="table">
-								<tr class="bg-primary"><th>学籍番号</th><th>学籍番号</th><th>学籍番号</th></tr>
-								<tr><td>名前</td><td>名前</td><td>名前</td></tr>
+								<tr class="bg-primary"><th>授業日数</th><th>出席数</th><th>欠席数</th></tr>
+								<tr><td>150</td><td>124</td><td>26</td></tr>
 							</table>
 						</div>
-						<div class="col-sm-6">
+						<div class="col-sm-6 all-attendance-rate-col">
 							<table class="table">
-								<tr class="bg-primary"><th>学籍番号</th><th>学籍番号</th><th>学籍番号</th></tr>
-								<tr><td>名前</td><td>名前</td><td>名前</td></tr>
+								<tr class="bg-primary"><th>遅刻数</th><th>不足</th><th>出席率</th></tr>
+								<tr><td>0</td><td>1</td><td>74.6%</td></tr>
 							</table>
 						</div>
 					</div>
@@ -58,13 +58,13 @@
 					<div class="text-center">
 						<h3 class="mainitem"><span class="mainitem glyphicon glyphicon-star" aria-hidden="true">講義別</span></h3>
 					</div>
-					<div class="visible-xs">
+					<div class="visible-xs text-center">
 						<select>
 							<option>ビジネスマナー</option>
 							<option>経営科学</option>
 						</select>
 					</div>
-					<div class="">
+					<div class="lesson-attendance">
 						<div class="row">
 							<div>
 								<div class="col-sm-6">
@@ -77,10 +77,10 @@
 								</div>
 								<div class="col-sm-6">
 									<table class="table">
-										<tr class="bg-primary"><th>学籍番号</th><th>学籍番号</th><th>学籍番号</th></tr>
-										<tr><td>名前</td><td>名前</td><td>名前</td></tr>
-										<tr class="bg-primary"><th>学籍番号</th><th>学籍番号</th><th>学籍番号</th></tr>
-										<tr><td>名前</td><td>名前</td><td>名前</td></tr>
+										<tr class="bg-primary"><th>授業数</th><th>不足</th><th>出席率</th></tr>
+										<tr><td>15</td><td>0</td><td>80.0％</td></tr>
+										<tr class="bg-primary"><th>出席</th><th>欠席</th><th>遅刻</th></tr>
+										<tr><td>12</td><td>3</td><td>0</td></tr>
 									</table>
 								</div>
 							</div>
