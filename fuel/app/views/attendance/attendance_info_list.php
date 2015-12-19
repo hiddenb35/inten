159026ec8c08ed6c4d9f65fddc33337a984cf53d
Keita Kasai <k013c1345@it-neec.jp>
2015-12-19 17:26:13 +0000

    issue#338 出席率一覧画面のコーディングを修正しました。

--- fuel/app/views/attendance/attendance_info_list.php
+++ fuel/app/views/attendance/attendance_info_list.php
@@ -1,178 +1,107 @@
 <!-- Main content -->
-<section id="ATTENDANCE_INFO_LIST" class="content clearfix">
-	<div class="container-fluid">
-
-		<div class="student-number">
-			<h3 class="number"><span>学籍番号</span>:K013C1296</h3>
-
-			<h3 class="name"><span>名前</span>:佐々木</h3>
-		</div>
-
-
-		<div class="menu form-group row">
-			<div class="col-lg-2 col-md-2 col-sm-2">
-			</div>
-			<div class="form-group col-lg-4 col-md-4 col-sm-4 col-xs-6">
-				<select class="form-control">
-					<option>1学年</option>
-					<option>2学年</option>
-					<option>3学年</option>
-					<option>4学年</option>
-					<option>5学年</option>
-				</select>
-			</div>
-			<div class="form-group col-lg-4 col-md-4 col-sm-4 col-xs-6">
-				<select class="form-control">
-					<option>前期</option>
-					<option>後期</option>
-				</select>
-			</div>
-			<div class="col-md-2 col-sm-2 col-lg-2">
-			</div>
-		</div>
-
-
-		<div>
-			<h3 class="mainitem"><span class="glyphicon glyphicon-star" aria-hidden="true">全体</span></h3>
-		</div>
-
-		<div class="row maintable">
-			<div class="col-lg-2 col-md-2 col-sm-4 col-xs-4">
-				<div class="row">
-					<div class="item col-lg-12 col-md-12 col-sm-12 col-xs-12">授業日数</div>
-					<div class="total col-lg-12 col-md-12 col-sm-12 col-xs-12">150</div>
-				</div>
-			</div>
-			<div class="col-lg-2 col-md-2 col-md-push-6 col-sm-4 col-xs-4">
-				<div class="row">
-					<div class="item col-lg-12 col-md-12 col-sm-12 col-xs-12">不足</div>
-					<div class="total col-lg-12 col-md-12 col-sm-12 col-xs-12">1</div>
-				</div>
-			</div>
-			<div class="col-lg-2 col-md-2 col-md-push-6 col-sm-4 col-xs-4">
-				<div class="row">
-					<div class="item col-lg-12 col-md-12 col-sm-12 col-xs-12">出席率</div>
-					<div class="bg-danger col-lg-12 total col-md-12 col-sm-12 col-xs-12">74.6%</div>
-				</div>
-			</div>
-			<div class="col-lg-2 col-md-2 col-md-pull-4 col-sm-4 col-xs-4">
-				<div class="row">
-					<div class="item col-lg-12 col-md-12 col-sm-12 col-xs-12">出席数</div>
-					<div class="total col-lg-12 col-md-12 col-sm-12 col-xs-12">124</div>
-				</div>
-			</div>
-			<div class="col-lg-2 col-md-2 col-md-pull-4 col-sm-4 col-xs-4">
-				<div class="row">
-					<div class="item col-lg-12 col-md-12 col-sm-12 col-xs-12">欠席数</div>
-					<div class="total col-lg-12 col-md-12 col-sm-12 col-xs-12">26</div>
-				</div>
-			</div>
-			<div class="col-lg-2 col-md-2 col-md-pull-4 col-sm-4 col-xs-4">
-				<div class="row">
-					<div class="item col-lg-12 col-md-12 col-sm-12 col-xs-12">遅刻数</div>
-					<div class="total col-lg-12 col-12 col-md-12 col-sm-12 col-xs-12">0</div>
-				</div>
-			</div>
-		</div>
-
-		<div>
-			<h3 class="mainitem"><span class="mainitem glyphicon glyphicon-star" aria-hidden="true">講義別</span></h3>
-		</div>
-
-		<div class="row menu form-group">
-			<div class="hidden-md hidden-lg col-sm-offset-3 col-sm-6 subject form-group">
-				<select class="form-control">
-					<option>ビジネスマナー</option>
-					<option>経営科学</option>
-				</select>
-			</div>
-		</div>
-
-		<div class="subject visible-md visible-lg"><h3><a href="#">ビジネスマナー</a></h3></div>
-
+<section id="ATTENDANCE_INFO_LIST" class="content">
+	<div class="">
 		<div class="row">
-			<div class="pie col-lg-offset-1 col-md-offset-1 col-lg-5 col-md-5">
-			</div>
-			<div class="col-md-5 col-lg-5">
-				<table class="table table-bordered table-striped table-hover table-condensed">
-					<thead>
-					<tr>
-						<th>授業数</th>
-						<th>不足</th>
-						<th>出席率</th>
-					</tr>
-					</thead>
-					<tbody>
-					<tr>
-						<td>15</td>
-						<td>0</td>
-						<td class="bg-danger">74.0%</td>
-					<tr>
-					</tbody>
-					<thead>
-					<tr>
-						<th>出席</th>
-						<th>欠席</th>
-						<th>遅刻</th>
-					<tr>
-						<tbody>
-					<tr>
-						<td>12</td>
-						<td>3</td>
-						<td>0</td>
-					<tr>
-					</tbody>
-				</table>
+			<div class="col-md-10 col-md-offset-1">
+				<div class="center-block" style="display:table;">
+					<div style="display: table-row">
+						<div style="display: table-cell;"><h3>学籍番号</h3></div>
+						<div style="display: table-cell;"><h3>:K013C1296</h3></div>
+					</div>
+					<div style="display: table-row">
+						<div style="display: table-cell;"><h3>名前</h3></div>
+						<div style="display: table-cell;"><h3>:佐々木aaaaa</h3></div>
+					</div>
+				</div>
+
+				<div class="text-center">
+					<div class="row">
+						<div class="col-xs-6">
+							<select>
+								<option>1学年</option>
+								<option>2学年</option>
+								<option>3学年</option>
+								<option>4学年</option>
+								<option>5学年</option>
+							</select>
+						</div>
+						<div class="col-xs-6">
+							<select>
+								<option>前期</option>
+								<option>後期</option>
+							</select>
+						</div>
+					</div>
+				</div>
+
+				<div class="info-box">
+					<div class="text-center">
+						<h3 class="mainitem"><span class="glyphicon glyphicon-star" aria-hidden="true">全体</span></h3>
+					</div>
+					<div class="row">
+						<div class="col-sm-6">
+							<table class="table">
+								<tr class="bg-primary"><th>学籍番号</th><th>学籍番号</th><th>学籍番号</th></tr>
+								<tr><td>名前</td><td>名前</td><td>名前</td></tr>
+							</table>
+						</div>
+						<div class="col-sm-6">
+							<table class="table">
+								<tr class="bg-primary"><th>学籍番号</th><th>学籍番号</th><th>学籍番号</th></tr>
+								<tr><td>名前</td><td>名前</td><td>名前</td></tr>
+							</table>
+						</div>
+					</div>
+				</div>
+
+				<div class="info-box">
+					<div class="text-center">
+						<h3 class="mainitem"><span class="mainitem glyphicon glyphicon-star" aria-hidden="true">講義別</span></h3>
+					</div>
+					<div class="visible-xs">
+						<select>
+							<option>ビジネスマナー</option>
+							<option>経営科学</option>
+						</select>
+					</div>
+					<div class="">
+						<div class="row">
+							<div>
+								<div class="col-sm-6">
+									<div class="col-xs-8">
+										<h3><a href="#">ビジネスマナー</a></h3>
+									</div>
+									<div class="col-xs-4">
+										<button type="button" class="btn btn-primary">詳細</button>
+									</div>
+								</div>
+								<div class="col-sm-6">
+									<table class="table">
+										<tr class="bg-primary"><th>学籍番号</th><th>学籍番号</th><th>学籍番号</th></tr>
+										<tr><td>名前</td><td>名前</td><td>名前</td></tr>
+										<tr class="bg-primary"><th>学籍番号</th><th>学籍番号</th><th>学籍番号</th></tr>
+										<tr><td>名前</td><td>名前</td><td>名前</td></tr>
+									</table>
+								</div>
+							</div>
+							<div class="">
+								<div class="col-sm-6">
+									<h3><a href="#">経営科学</a></h3>
+								</div>
+								<div class="col-sm-6">
+									<button type="button" class="btn btn-primary">詳細</button>
+									<table class="table">
+										<tr class="bg-primary"><th>学籍番号</th><th>学籍番号</th><th>学籍番号</th></tr>
+										<tr><td>名前</td><td>名前</td><td>名前</td></tr>
+										<tr class="bg-primary"><th>学籍番号</th><th>学籍番号</th><th>学籍番号</th></tr>
+										<tr><td>名前</td><td>名前</td><td>名前</td></tr>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
-
-		<div class="row">
-			<button type="button" class="button col-md-offset-7 col-md-3 col-xs-offset-8 col-xs-3 btn btn-primary">詳細
-			</button>
-		</div>
-		<div class="subject visible-md visible-lg"><h3><a href="#">経営科学</a></h3></div>
-
-		<div class="row">
-			<div class="pie col-md-offset-1 col-md-5">
-			</div>
-			<div class="col-md-5">
-				<table class="table table-bordered table-striped table-hover table-condensed">
-					<thead>
-					<tr>
-						<th>授業数</th>
-						<th>不足</th>
-						<th>出席率</th>
-					</tr>
-					</thead>
-					<tbody>
-					<tr>
-						<td>15</td>
-						<td>0</td>
-						<td>80.0%</td>
-					<tr>
-					</tbody>
-					<thead>
-					<tr>
-						<th>出席</th>
-						<th>欠席</th>
-						<th>遅刻</th>
-					<tr>
-						<tbody>
-					<tr>
-						<td>12</td>
-						<td>3</td>
-						<td>0</td>
-					<tr>
-					</tbody>
-				</table>
-			</div>
-		</div>
-
-		<div class="row">
-			<button type="button" class="button col-md-offset-7 col-md-3 col-xs-offset-8 col-xs-3 btn btn-primary">詳細
-			</button>
-		</div>
-
 	</div>
-
 </section>
