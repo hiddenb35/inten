bde9b43b755d83e005bf468c7c45be84bc44cbf5
Keita Kasai <k013c1345@it-neec.jp>
2015-12-24 16:56:14 +0000

    issue#341 グラフを表示するようにしました。

--- fuel/app/views/attendance/attendance_rate_detail.php
+++ fuel/app/views/attendance/attendance_rate_detail.php
@@ -17,9 +17,9 @@
                 <div class="text-center">
                     <h3 class=""><span class="glyphicon glyphicon-star" aria-hidden="true">ビジネスマナー</span></h3>
                 </div>
-                <div class="col-sm-6 hidden-xs"></div>
+                <div class="col-sm-6 hidden-xs" id="highcharts3d"></div>
                 <div class="col-sm-6">
-                    <table class="table">
+                    <table class="table" style="margin-top: 10%;">
                         <tr class="bg-primary"><th>授業数</th><th>不足</th><th>出席率</th></tr>
                         <tr><td>15</td><td>0</td><td>80.0％</td></tr>
                         <tr class="bg-primary"><th>出席</th><th>欠席</th><th>遅刻</th></tr>
@@ -28,7 +28,7 @@
                 </div>
 
                 <div class="attendance-log">
-                    <div class="table-responsive">
+                    <div class="">
                         <table class="table">
                             <tr class="bg-primary"><th></th><th>1時限</th><th>2時限</th><th>3時限</th><th>4時限</th><th>5時限</th><th>6時限</th></tr>
                             <tr><th>4/1</th><td>出席</td><td>出席</td><td>出席</td><td>出席</td><td>出席</td><td>欠席</td></tr>
@@ -44,3 +44,52 @@
         </div>
     </div>
 </section>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/4.2.0/highcharts.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/4.2.0/highcharts-3d.js"></script>
+<script>
+    /************ Hifhcharts-3d.js ***********/
+    var h_data = [
+        {name: "出席", y: 80, sliced: true, selected: true},
+        {name: "遅刻", y: 9},
+        {name: "欠席", y: 6},
+        {name: "公欠", y: 3},
+        {name: "休校", y: 2}
+    ];
+    $('#highcharts3d').highcharts({
+        title: {text: ''},
+        colors: ['#0f0','#ff0', '#f00', '#00f', '#0ff'],
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                depth: 35,
+                dataLabels: {
+                    formatter: function() {
+                        return '<b>'+ this.point.name +'</b>:'+ this.percentage + '%';
+                    }
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: '',
+            data: h_data
+        }],
+        tooltip: {
+            formatter: function() {
+                return this.y +'%';},
+            enabled:true
+        },
+        chart: {
+            type: 'pie',
+            options3d: {
+                enabled: true,
+                alpha: 45,
+                beta: 0
+            },
+            backgroundColor: 'transparent',
+            height: 300
+        },
+        credits: {enabled: false}
+    });
+</script>
