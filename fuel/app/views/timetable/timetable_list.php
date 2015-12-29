0bf223679b1bf82e5e53474a2135e4d1ab33ff21
Shota Narumi <k013c1112@it-neec.jp>
2015-12-29 17:18:57 +0900

    issue#349 自分の担当するクラスの時間割一覧画面のコード修正
    ・グリッドシステムのためのクラス追加

--- fuel/app/views/timetable/timetable_list.php
+++ fuel/app/views/timetable/timetable_list.php
@@ -5,19 +5,19 @@
 			<table class="table table-bordered table-striped" id="college_list_table">
 				<tbody>
 				<tr>
-					<th>時間割名</th>
-					<th>作成日</th>
-					<th>更新日</th>
-					<th>ステータス</th>
-					<th></th>
+					<th class="col-xs-5 col-sm-2">時間割名</th>
+					<th class="col-sm-3 hidden-xs">作成日</th>
+					<th class="col-sm-3 hidden-xs">更新日</th>
+					<th class="col-xs-3 col-sm-2">ステータス</th>
+					<th class="col-xs-4 col-sm-2"></th>
 				</tr>
 				<?php foreach($timetable_lists as $timetable): ?>
 					<tr>
 						<td><?php echo $timetable['name']; ?></td>
-						<td><?php echo $timetable['created_at']; ?></td>
-						<td><?php echo $timetable['updated_at']; ?></td>
+						<td class="hidden-xs"><?php echo $timetable['created_at']; ?></td>
+						<td class="hidden-xs"><?php echo $timetable['updated_at']; ?></td>
 						<td><?php echo $timetable['status']; ?></td>
-						<td>
+						<td class="text-center">
 							<a href="<?php echo $timetable['edit_link']; ?>" class="btn btn-primary btn-sm">編集</a>
 							<a href="<?php echo $timetable['delete_link']; ?>" class="btn btn-danger btn-sm">削除</a>
 						</td>
