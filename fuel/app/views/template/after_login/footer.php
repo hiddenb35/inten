852647f5a3a4fc29240936972c225df060775e93
TKato <k013c1140@it-neec.jp>
2016-01-06 20:01:30 +0900

    issue#357 ロード画面の記述場所の変更

--- fuel/app/views/template/after_login/footer.php
+++ fuel/app/views/template/after_login/footer.php
@@ -2,3 +2,9 @@
 	<b>Version</b> 2.2.0
 </div>
 <strong>Copyright &copy; 2015 narumi.</strong> All rights reserved.
+<div id="loader-bg">
+	<div id="loader">
+		<img src="/assets/img/img-loading.gif" width="80" height="80" alt="Now Loading..." />
+		<p>Now Loading...</p>
+	</div>
+</div>
