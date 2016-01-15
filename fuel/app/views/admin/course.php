a4c75cd0dfd13ffdbf9f58908b2ace967c143c4c
Keita Kasai <k013c1345@it-neec.jp>
2016-01-15 23:18:17 +0900

    issue#364 学科一覧の追加フォームの修正

--- fuel/app/views/admin/course.php
+++ fuel/app/views/admin/course.php
@@ -36,25 +36,37 @@
 						<form action="/admin/course/add#course_form" method="post" role="form" class="form-horizontal"
 							  id="course_form">
 							<div class="row">
-								<div class="form-group" id="form_course_add">
-									<label for="name" class="col-sm-1 control-label">学科名</label>
-									<div class="col-sm-2">
+								<div class="col-sm-5" id="form_course_add">
+									<label for="name" class="control-label col-sm-4">学科名</label>
+									<div class="col-sm-8">
 										<input type="text" class="form-control" id="name" name="name" value="<?php if (isset($inputs['name'])) { echo $inputs['name']; }; ?>">
 									</div>
-									<label for="college_id" class="col-sm-1 control-label">カレッジ</label>
-									<div class="col-sm-2">
+								</div>
+								<div class="col-sm-5">
+									<label for="college_id" class="control-label col-sm-4">カレッジ</label>
+									<div class="col-sm-8">
 										<select id="college_id" name="college_id" class="form-control">
 											<?php foreach ($college_lists as $college_list): ?>
 												<option value="<?php echo $college_list['id'] ?>"><?php echo $college_list['name'] ?></option>
 											<?php endforeach; ?>
 										</select>
 									</div>
-									<label for="code" class="col-sm-1 control-label">学科コード</label>
-									<div class="col-sm-1">
+								</div>
+							</div>
+							<div class="row">
+								<div class="text-danger col-sm-3 col-sm-offset-1"><?php if (isset($errors['name'])) { echo $errors['name']; }; ?></div>
+								<div class="text-danger col-sm-3"><?php if (isset($errors['college_id'])) { echo $errors['college_id']; }; ?></div>
+							</div>
+							<div class="row course-add-second">
+								<div class="col-sm-5">
+									<label for="code" class="control-label col-sm-4">学科コード</label>
+									<div class="col-sm-3">
 										<input type="text" id="code" name="code" class="form-control" value="<?php if (isset($inputs['code'])) { echo $inputs['code']; }; ?>">
 									</div>
-									<label for="year_system" class="col-sm-1 control-label">年制</label>
-									<div class="col-sm-1">
+								</div>
+								<div class="col-sm-5">
+									<label for="year_system" class="control-label col-sm-4">年制</label>
+									<div class="col-sm-4">
 										<select id="year_system" name="year_system" class="form-control">
 											<option value="1">1</option>
 											<option value="2">2</option>
@@ -62,15 +74,14 @@
 											<option value="4">4</option>
 										</select>
 									</div>
-									<div class="form-button col-sm-2">
-										<button type="submit" class="btn btn-primary">登録</button>
-										<button type="reset" class="btn btn-warning">キャンセル</button>
-									</div>
 								</div>
-								<div class="row">
-									<div class="text-danger col-sm-3 col-sm-offset-1"><?php if (isset($errors['name'])) { echo $errors['name']; }; ?></div>
-									<div class="text-danger col-sm-3"><?php if (isset($errors['college_id'])) { echo $errors['college_id']; }; ?></div>
-									<div class="text-danger col-sm-4"><?php if (isset($errors['code'])) { echo $errors['code']; }; ?></div>
+							</div>
+							<div class="row">
+								<div class="text-danger col-sm-4"><?php if (isset($errors['code'])) { echo $errors['code']; }; ?></div>
+							</div>
+							<div class="row">
+								<div class="form-button col-sm-1 col-sm-offset-5 col-xs-offset-4 register-button">
+									<button type="submit" class="btn btn-primary">登録</button>
 								</div>
 							</div>
 						</form>
