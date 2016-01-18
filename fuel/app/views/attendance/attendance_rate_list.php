3458567cd2543d8d41e7264af6c5caea781062e0
Shota Narumi <k013c1112@it-neec.jp>
2016-01-18 11:12:21 +0900

    issue#358 htmlの修正
    出席率一覧画面
    ・テーブル幅を狭くしました

--- fuel/app/views/attendance/attendance_rate_list.php
+++ fuel/app/views/attendance/attendance_rate_list.php
@@ -2,30 +2,32 @@
 <section id="ATTENDANCE_RATE_LIST" class="content">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="table-responsive">
-				<table class="table table-bordered table-striped table-hover table-condensed">
-					<thead>
-					<tr>
-						<th class="col-sm-3 col-xs-5">学籍番号</th>
-						<th class="col-sm-3 col-xs-5">名前</th>
-						<th class="col-sm-3 hidden-xs">フリガナ</th>
-						<th colspan="2" class="col-sm-3 col-xs-2">出席率</th>
-					</tr>
-					</thead>
-					<tbody>
-					<?php foreach($student_lists as $student_list): ?>
+			<div class="col-md-offset-1 col-md-10">
+				<div class="table-responsive">
+					<table class="table table-bordered table-striped table-hover table-condensed">
+						<thead class="row">
 						<tr>
-							<td><?php echo $student_list['number']; ?></td>
-							<td><?php echo $student_list['full_name']; ?></td>
-							<td class="hidden-xs"><?php echo $student_list['full_name_kana']; ?></td>
-							<td class="hidden-xs progress">
-								<div class="progress-bar <?php echo $student_list['rate_bar_class']; ?>" style="width: <?php echo $student_list['rate']; ?>;"></div>
-							</td>
-							<td class="text-center"><span class="badge <?php echo $student_list['rate_bg_class']; ?>"><?php echo $student_list['rate']; ?></span></td>
+							<th class="col-sm-3 col-xs-5">学籍番号</th>
+							<th class="col-sm-3 col-xs-5">名前</th>
+							<th class="col-sm-3 hidden-xs">フリガナ</th>
+							<th colspan="2" class="col-sm-3 col-xs-2">出席率</th>
 						</tr>
-					<?php endforeach; ?>
-					</tbody>
-				</table>
+						</thead>
+						<tbody>
+						<?php foreach($student_lists as $student_list): ?>
+							<tr>
+								<td><?php echo $student_list['number']; ?></td>
+								<td><?php echo $student_list['full_name']; ?></td>
+								<td class="hidden-xs"><?php echo $student_list['full_name_kana']; ?></td>
+								<td class="hidden-xs progress">
+									<div class="progress-bar <?php echo $student_list['rate_bar_class']; ?>" style="width: <?php echo $student_list['rate']; ?>;"></div>
+								</td>
+								<td class="text-center"><span class="badge <?php echo $student_list['rate_bg_class']; ?>"><?php echo $student_list['rate']; ?></span></td>
+							</tr>
+						<?php endforeach; ?>
+						</tbody>
+					</table>
+				</div>
 			</div>
 		</div>
 	</div>
