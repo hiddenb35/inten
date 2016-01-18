02d96c0afd0604bb2b527befa9a0da527d849102
Shota Narumi <k013c1112@it-neec.jp>
2016-01-18 10:38:26 +0900

    issue#358 htmlの修正
    出席を取る画面
    ・送信、リセットボタンの修正

--- fuel/app/views/attendance/take_attendance.php
+++ fuel/app/views/attendance/take_attendance.php
@@ -76,9 +76,9 @@
 				<?php endforeach; ?>
 			</div>
 		</div>
-		<div class="text-center">
-			<button type="submit" class="btn btn-primary">送信</button>
-			<button type="reset" class="btn btn-danger">リセット</button>
+		<div class="text-center row">
+			<button type="submit" class="btn btn-primary col-xs-offset-3 col-xs-3 col-sm-offset-4 col-sm-2">送信</button>
+			<button type="reset" class="btn btn-danger col-xs-3 col-sm-2">リセット</button>
 		</div>
 		<!-- popup -->
 		<div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
