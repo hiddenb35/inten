393454fca599be8118b582ebaaf81ebe956f61c1
Shota Narumi <k013c1112@it-neec.jp>
2016-01-25 11:17:29 +0900

    issue#384 クラス一覧のボックスデザインの変更

--- fuel/app/views/attendance/new_responsible_list.php
+++ fuel/app/views/attendance/new_responsible_list.php
@@ -4,14 +4,15 @@
 		<div class="row">
 			<?php foreach($class_lists as $class_list): ?>
 				<div class="col-sm-6 col-md-4 col-lg-3">
-					<div class="box text-center">
+					<div class="box clearfix relative">
 						<a href="<?php echo $class_list['link_url']; ?>">
-						<div class="box-body">
-							<div class="class_name h2"><?php echo $class_list['name']; ?></div>
+						<div class="pull-left absolute">
+							<div class="class_name h3"><?php echo $class_list['name']; ?></div>
 							<div class="college_name"><?php echo $class_list['college_name']; ?></div>
 							<div class="department_name"><?php echo $class_list['course_name']; ?></div>
 							<div class="class_member">人数：<?php echo $class_list['student_sum']; ?>名</div>
 						</div>
+						<img src="/assets/img/green-square.png" alt="" class="pull-right">
 					</div>
 				</div>
 			<?php endforeach; ?>
