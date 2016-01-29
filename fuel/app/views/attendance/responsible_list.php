d04f84306242eefa9c2540fd0a14181be4e0d67d
Shota Narumi <k013c1112@it-neec.jp>
2016-01-29 10:23:07 +0900

    issue#394 boxの修正
    #responsible_list

--- fuel/app/views/attendance/responsible_list.php
+++ fuel/app/views/attendance/responsible_list.php
@@ -4,17 +4,15 @@
 		<div class="row">
 			<?php foreach($lesson_lists as $lesson): ?>
 				<div class="col-sm-6 col-md-4">
-					<div class="box">
-						<a href=<?php echo $lesson['link_url']; ?>>
-							<div class="box-body clearfix relative">
-								<div class="pull-left absolute">
-									<div class="lesson_name h3"><?php echo $lesson['name']; ?></div>
-									<div class="class_name h4"><?php echo $lesson['class_name']; ?></div>
-								</div>
-								<img src="/assets/img/box.png" alt="" class="pull-right">
+					<a href=<?php echo $lesson['link_url']; ?>>
+						<div class="lesson-box clearfix relative">
+							<div class="pull-left absolute">
+								<div class="lesson_name h3"><?php echo $lesson['name']; ?></div>
+								<div class="class_name h4"><?php echo $lesson['class_name']; ?></div>
 							</div>
-						</a>
-					</div>
+							<img src="/assets/img/box.png" alt="" class="pull-right">
+						</div>
+					</a>
 				</div>
 			<?php endforeach; ?>
 		</div>
