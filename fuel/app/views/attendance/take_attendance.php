d518e443a1dc5d892dcea067b0149da12bb2bb27
Shota Narumi <k013c1112@it-neec.jp>
2016-01-29 16:44:47 +0900

    issue#395 出席を取る画面の修正

--- fuel/app/views/attendance/take_attendance.php
+++ fuel/app/views/attendance/take_attendance.php
@@ -18,62 +18,45 @@
 			</div>
 		</div>
 		<input type="hidden" name="lesson_id" value="<?php echo $lesson_info['id']; ?>">
-		<div class="row pc-size">
-			<?php foreach($student_lists as $index => $student): ?>
-			<div class="col-sm-6 col-md-4 col-lg-3">
-				<div class="box">
-					<div class="box-header">
-						<input type="hidden" name="attendance[<?php echo $index; ?>][student_id]" value="<?php echo $student['id']; ?>">
-						<input type="hidden" class="status" name="attendance[<?php echo $index; ?>][status]">
-						<h3 class="box-title"><?php echo $student['number']; ?></h3>
-						<div class="kana"><?php echo $student['full_name_kana']; ?></div>
-						<div class="box-tools pull-right">
-							<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-							</button>
-						</div>
-					</div>
-					<div class="box-content">
-						<div class="h3 text-center"><?php echo $student['full_name']; ?></div>
-					</div>
-					<div class="box-footer text-center">
-						<button type="button" class="btn btn-primary abtn" data-status-code="1">出席</button>
-						<button type="button" class="btn btn-warning abtn" data-status-code="2">遅刻</button>
-						<button type="button" class="btn btn-danger abtn" data-status-code="3">欠席</button>
-					</div>
-				</div>
-			</div>
-			<?php endforeach; ?>
-		</div>
-		<!------------------------スマホ・タブレットサイズ-------------------------------->
 		<div class="sp-size">
-			<div class="text-center mtop"><!--mtop margin-top取ってます-->
-				<button type="button" class="btn take-attendance" data-toggle="modal" data-target="#myModal2" id="attendance">出席を取る</button>
+			<div class="text-center mtop row"><!--mtop margin-top取ってます-->
+				<div class="col-sm-offset-2 col-sm-8">
+					<button type="button" class="btn take-attendance" data-toggle="modal" data-target="#myModal2" id="attendance">出席を取る</button>
+				</div>
 			</div>
-			<div class="panel-group" id="accordion">
-				<?php foreach($student_lists as $index => $student): ?>
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<div class="panel-title h4">
-							<!--------------学生の数だけstudentの数値が増加する---------------->
-							<div class="text-center" data-toggle="collapse" data-parent="#accordion" href="#student<?php echo $index; ?>">
-								<div class="cell create-circle"></div>
-								<div class="cell student-info"><?php echo $student['number']; ?> <?php echo $student['full_name']; ?></div>
-								<ul class="cell hidden-student-info">
-									<li><?php echo $student['number']; ?></li>
-									<li><?php echo $student['full_name']; ?></li>
-								</ul>
+			<div class="row">
+				<div class="col-sm-offset-2 col-sm-8">
+					<div class="panel-group box box-main" id="accordion">
+						<div class="box-body">
+							<?php foreach($student_lists as $index => $student): ?>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<div class="panel-title h4">
+										<!--------------学生の数だけstudentの数値が増加する---------------->
+										<div class="text-center" data-toggle="collapse" data-parent="#accordion" href="#student<?php echo $index; ?>">
+											<div class="cell create-circle"></div>
+											<div class="cell student-info"><?php echo $student['number']; ?></div>
+											<div class="cell student-info"><?php echo $student['full_name']; ?></div>
+											<div class="cell student-info hidden-xs hidden-sm"><?php echo $student['full_name_kana']; ?></div>
+											<ul class="cell hidden-student-info">
+												<li><?php echo $student['number']; ?></li>
+												<li><?php echo $student['full_name']; ?></li>
+											</ul>
+										</div>
+									</div>
+								</div>
+								<div id="student<?php echo $index; ?>" class="panel-collapse collapse">
+									<div class="panel-body text-center row">
+										<button type="button" class="btn btn-primary" data-status-code="1">出席</button>
+										<button type="button" class="btn btn-warning" data-status-code="2">遅刻</button>
+										<button type="button" class="btn btn-danger" data-status-code="3">欠席</button>
+									</div>
+								</div>
 							</div>
-						</div>
-					</div>
-					<div id="student<?php echo $index; ?>" class="panel-collapse collapse">
-						<div class="panel-body text-center">
-							<button type="button" class="btn btn-primary" data-status-code="1">出席</button>
-							<button type="button" class="btn btn-warning" data-status-code="2">遅刻</button>
-							<button type="button" class="btn btn-danger" data-status-code="3">欠席</button>
+							<?php endforeach; ?>
 						</div>
 					</div>
 				</div>
-				<?php endforeach; ?>
 			</div>
 		</div>
 		<div class="text-center row">
