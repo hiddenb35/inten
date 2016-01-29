46394abd382d22fb9a80572679fcc4a2e8cdd6d2
Shota Narumi <k013c1112@it-neec.jp>
2016-01-29 13:06:23 +0900

    issue#394 boxの修正
    #new_responsible_list

--- fuel/app/views/attendance/new_responsible_list.php
+++ fuel/app/views/attendance/new_responsible_list.php
@@ -3,17 +3,17 @@
 	<div class="container-fluid">
 		<div class="row">
 			<?php foreach($class_lists as $class_list): ?>
-				<div class="col-sm-6 col-md-4 col-lg-3">
-					<div class="box clearfix relative">
-						<a href="<?php echo $class_list['link_url']; ?>">
-						<div class="pull-left absolute">
-							<div class="class_name h3"><?php echo $class_list['name']; ?></div>
-							<div class="college_name"><?php echo $class_list['college_name']; ?></div>
-							<div class="department_name"><?php echo $class_list['course_name']; ?></div>
-							<div class="class_member">人数：<?php echo $class_list['student_sum']; ?>名</div>
+				<div class="col-sm-6 col-md-4">
+					<a href="<?php echo $class_list['link_url']; ?>">
+						<div class="class-box clearfix relative">
+							<div class="pull-left absolute">
+								<div class="class_name h3"><?php echo $class_list['name']; ?></div>
+								<div class="college_name"><?php echo $class_list['college_name']; ?></div>
+								<div class="department_name"><?php echo $class_list['course_name']; ?>(<?php echo $class_list['student_sum']; ?>名)</div>
+							</div>
+							<img src="/assets/img/box.png" alt="" class="pull-right">
 						</div>
-						<img src="/assets/img/green-square.png" alt="" class="pull-right">
-					</div>
+					</a>
 				</div>
 			<?php endforeach; ?>
 		</div>
